6870e99ec75850b4de587e4680670295d418ce9c
ghuazh <ghuazh@gmail.com>
2013-08-15 11:19:26 +0800

    Signed-off-by: ghuazh <ghuazh@gmail.com>

--- com.sg.business.model/src/com/sg/business/model/dataset/organization/RoleOfOrgIterationOption.java
+++ com.sg.business.model/src/com/sg/business/model/dataset/organization/RoleOfOrgIterationOption.java
@@ -1,69 +1,25 @@
 package com.sg.business.model.dataset.organization;
 
-import java.util.List;
-
-import com.mobnut.db.model.DataSet;
 import com.mobnut.db.model.PrimaryObject;
-import com.sg.widgets.commons.dataset.IOptionDataSetFactory;
+import com.mongodb.BasicDBObject;
+import com.sg.business.model.IModelConstants;
+import com.sg.business.model.ProjectTemplate;
+import com.sg.business.model.RoleDefinition;
+import com.sg.business.model.WorkDefinition;
+import com.sg.widgets.commons.dataset.OptionDataSetFactory;
 
-public class RoleOfOrgIterationOption implements IOptionDataSetFactory {
+public class RoleOfOrgIterationOption extends OptionDataSetFactory {
 
 	public RoleOfOrgIterationOption() {
-		// TODO Auto-generated constructor stub
-	}
-
-	@Override
-	public List<PrimaryObject> doQuery(DataSet ds) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public long getTotalCount() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public DataSet getDataSet() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void setFieldData(PrimaryObject fieldData) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public PrimaryObject getFieldData() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void setFieldName(String fieldName) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public String getFieldName() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public PrimaryObject doFindOne(Object value) {
-		// TODO Auto-generated method stub
-		return null;
+		super(IModelConstants.DB, IModelConstants.C_ROLE_DEFINITION);
 	}
 
 	@Override
 	public void setEditorData(PrimaryObject data) {
-		// TODO Auto-generated method stub
-
+		WorkDefinition workd = (WorkDefinition) data;
+		ProjectTemplate projectTemplate = workd.getProjectTemplate();
+		setQueryCondition(new BasicDBObject().append(
+				RoleDefinition.F_PROJECT_TEMPLATE_ID, projectTemplate.get_id()));
 	}
 
 }
