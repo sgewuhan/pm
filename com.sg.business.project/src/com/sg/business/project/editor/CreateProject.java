618b3077f8bd7fe177ad2ba58d46f0b573449721
ghuazh <ghuazh@gmail.com>
2013-08-20 13:47:38 +0800

    Signed-off-by: ghuazh <ghuazh@gmail.com>

--- com.sg.business.project/src/com/sg/business/project/editor/CreateProject.java
+++ com.sg.business.project/src/com/sg/business/project/editor/CreateProject.java
@@ -1,6 +1,7 @@
 package com.sg.business.project.editor;
 
 import org.eclipse.jface.viewers.ColumnViewer;
+import org.eclipse.jface.viewers.IInputProvider;
 import org.eclipse.jface.viewers.IStructuredSelection;
 
 import com.mobnut.db.model.AccountInfo;
@@ -8,7 +9,6 @@ import com.mobnut.db.model.ModelService;
 import com.mobnut.portal.user.UserSessionContext;
 import com.mongodb.BasicDBObject;
 import com.sg.business.model.Project;
-import com.sg.widgets.part.editor.DataObjectEditor;
 import com.sg.widgets.part.editor.DataObjectWizard;
 import com.sg.widgets.viewer.ICreateEditorDelegator;
 
@@ -16,14 +16,14 @@ public class CreateProject implements ICreateEditorDelegator {
 
 
 	@Override
-	public DataObjectEditor create(IStructuredSelection selection,
+	public IInputProvider create(IStructuredSelection selection,
 			String editorId, ColumnViewer viewer) throws Exception {
 		Project po = ModelService.createModelObject(new BasicDBObject(), Project.class);
 		AccountInfo ac = UserSessionContext.getAccountInfo();
 		//默认的项目经理是当前的用户
 		po.setValue(Project.F_CHARGER, ac.getUserId());
-		DataObjectWizard.open(po, editorId, true, null);
-		return null;
+		DataObjectWizard w = DataObjectWizard.open(po, editorId, true, null);
+		return w;
 	}
 
 }
