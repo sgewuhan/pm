667c1ef7c06a8af4a963b5f433c59ac197e26a21
jinxitao <jxt@yaozheng.com.cn>
2013-09-05 22:19:06 +0800

    Signed-off-by: jinxitao <jxt@yaozheng.com.cn>

---
+++ com.sg.business.message/src/com/sg/business/message/view/Wastebox.java
@@ -0,0 +1,24 @@
+package com.sg.business.message.view;
+
+import com.mobnut.portal.user.UserSessionContext;
+import com.sg.widgets.part.view.AccountSensitiveTableView;
+
+public class Wastebox extends AccountSensitiveTableView {
+
+	@Override
+	public void accountChanged(Object data) {
+		//只响应以下的事件
+		if (UserSessionContext.EVENT_CONSIGNER_CHANGED.equals(data)
+				|| UserSessionContext.EVENT_MESSAGE.equals(data)
+			) {
+			super.accountChanged(data);
+		}
+	}
+
+	@Override
+	protected String getAccountNoticeMessage() {
+		return "重新读取收件信息。。。";
+	}
+
+
+}
