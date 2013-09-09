fd112ca85542716c3dd9e587ad8b849803ea2e8e
yangjun1985 <gdiyang@lode>
2013-09-09 19:27:10 +0800

    Signed-off-by: yangjun1985 <gdiyang@lode>

--- com.sg.business.organization/src/com/sg/business/organization/command/SyncHRHandler.java
+++ com.sg.business.organization/src/com/sg/business/organization/command/SyncHRHandler.java
@@ -3,26 +3,64 @@ package com.sg.business.organization.command;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.runtime.Assert;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.handlers.HandlerUtil;
 
+import com.sg.business.model.AbstractWork;
+import com.sg.widgets.MessageUtil;
+import com.sg.widgets.Widgets;
+import com.sg.widgets.part.IEditablePart;
 import com.sg.widgets.part.IRefreshablePart;
+import com.sg.widgets.part.editor.DataObjectDialog;
+import com.sg.widgets.registry.config.Configurator;
+import com.sg.widgets.registry.config.DataEditorConfigurator;
+import com.sg.widgets.viewer.ViewerControl;
 
 public class SyncHRHandler extends AbstractHandler {
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		long start = System.currentTimeMillis();
-
-		// 与HR的组织进行同步
-		SyscHR syscHR = new SyscHR();
-		syscHR.doSyscHROrganization();
-
-		IWorkbenchPart part = HandlerUtil.getActivePart(event);
-		if (part instanceof IRefreshablePart) {
-			((IRefreshablePart) part).doRefresh();
-		}
-		System.out.println(System.currentTimeMillis() - start);
+//		long start = System.currentTimeMillis();
+//
+//		// 与HR的组织进行同步
+//		SyscHR syscHR = new SyscHR();
+//		syscHR.doSyscHROrganization();
+//
+//		IWorkbenchPart part = HandlerUtil.getActivePart(event);
+//		if (part instanceof IRefreshablePart) {
+//			((IRefreshablePart) part).doRefresh();
+//		}
+//		System.out.println(System.currentTimeMillis() - start);
+		
+//		Shell shell = HandlerUtil.getActiveShell(event);
+//
+//		AbstractWork po = ((AbstractWork) selected).makeChildWork();
+//		ViewerControl vc = getCurrentViewerControl(event);
+//		Assert.isNotNull(vc);
+//
+//		// 以下两句很重要，使树currentViewerControl够侦听到保存事件， 更新树上的节点
+//		// 1. 设置父并不涉及到数据库，主要是维护模型的父子关系，同时ViewerControl也需要父来处理事件响应
+//		po.setParentPrimaryObject(selected);
+//		// 2. 侦听po的事件作出合适的响应
+//		po.addEventListener(vc);
+//
+//		// 使用编辑器打开编辑工作定义
+//		Configurator conf = Widgets.getEditorRegistry().getConfigurator(
+//				po.getDefaultEditorId());
+//		try {
+//			DataObjectDialog.openDialog(po, (DataEditorConfigurator) conf,
+//					true, null,"创建"+po.getTypeName());
+//		} catch (Exception e) {
+//			MessageUtil.showToast(shell, "创建"+po.getTypeName(), e.getMessage(),
+//					SWT.ICON_ERROR);
+//		}
+//
+//		// 3. 处理完成后，释放侦听器
+//		po.removeEventListener(vc);
+		
 		return null;
 	}
 
---
+++ com.sg.business.organization/src/com/sg/business/organization/dialog/SyncHRDialog.java
@@ -0,0 +1,143 @@
+package com.sg.business.organization.dialog;
+
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.dialogs.IMessageProvider;
+import org.eclipse.jface.viewers.IInputProvider;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.MenuItem;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.forms.FormDialog;
+import org.eclipse.ui.forms.IManagedForm;
+import org.eclipse.ui.forms.IMessage;
+import org.eclipse.ui.forms.events.HyperlinkAdapter;
+import org.eclipse.ui.forms.events.HyperlinkEvent;
+import org.eclipse.ui.forms.widgets.ScrolledForm;
+
+import com.sg.widgets.part.internal.MultipageEditablePanel;
+
+public class SyncHRDialog extends FormDialog implements IInputProvider {
+
+	protected Object input;
+	protected Font font;
+	private String title;
+	protected IManagedForm managedForm;
+	protected ScrolledForm form;
+	protected Menu messagePopup;
+
+	public SyncHRDialog(Shell shell, Object input, String title) {
+		super(shell);
+		this.input = input;
+		this.title = title;
+	}
+
+	@Override
+	public boolean close() {
+		font.dispose();
+		return super.close();
+	}
+
+	private void loadDialogTitle() {
+		getShell().setText(title);
+	}
+
+	@Override
+	protected void createButtonsForButtonBar(Composite parent) {
+
+		createButton(parent, IDialogConstants.OK_ID,
+				IDialogConstants.get().OK_LABEL, true);
+		createButton(parent, IDialogConstants.CANCEL_ID,
+				IDialogConstants.get().CANCEL_LABEL, false);
+	}
+
+	@Override
+	protected void createFormContent(IManagedForm managedForm) {
+		this.managedForm = managedForm;
+		form = managedForm.getForm();
+		font = new Font(form.getDisplay(), "Î¢ÈíÑÅºÚ", 16, SWT.NORMAL); //$NON-NLS-1$
+
+		managedForm.setInput(input);
+
+		addFormMessageLisener(managedForm);
+
+		// ´´½¨page body
+		createContent(form.getBody());
+		form.reflow(true);
+	}
+
+	protected void createContent(Composite parent) {
+		loadDialogTitle();
+
+		parent.setLayout(new FillLayout());
+
+		MultipageEditablePanel folder = new MultipageEditablePanel(parent,
+				SWT.TOP | SWT.FLAT);
+		folder.setMessageManager(form.getMessageManager());
+		// folder.createContents(managedForm, input);
+
+	}
+
+	private void addFormMessageLisener(IManagedForm managedForm) {
+		form.getForm().addMessageHyperlinkListener(new HyperlinkAdapter() {
+			public void linkActivated(HyperlinkEvent e) {
+				showMessagePopup(e);
+			}
+		});
+	}
+
+	private void showMessagePopup(HyperlinkEvent e) {
+		if (messagePopup != null && !messagePopup.isDisposed()) {
+			messagePopup.dispose();
+		}
+		messagePopup = new Menu(form.getShell(), SWT.POP_UP);
+		IMessage[] messages = form.getForm().getChildrenMessages();
+		for (int i = 0; i < messages.length; i++) {
+			final IMessage message = messages[i];
+			MenuItem item = new MenuItem(messagePopup, SWT.PUSH);
+			item.setText(message.getPrefix() + " " + message.getMessage()); //$NON-NLS-1$
+			item.setImage(getMessageImage(message.getMessageType()));
+			item.addSelectionListener(new SelectionAdapter() {
+				@Override
+				public void widgetSelected(SelectionEvent e) {
+					Control c = message.getControl();
+					if (c != null && !c.isDisposed()) {
+						c.setFocus();
+					}
+				}
+			});
+		}
+		Point hl = ((Control) e.widget).toDisplay(0, 0);
+		hl.y += 16;
+		messagePopup.setLocation(hl);
+		messagePopup.setVisible(true);
+
+	}
+
+	private Image getMessageImage(int type) {
+		Display display = form.getShell().getDisplay();
+		switch (type) {
+		case IMessageProvider.ERROR:
+			return display.getSystemImage(SWT.ICON_ERROR);
+		case IMessageProvider.WARNING:
+			return display.getSystemImage(SWT.ICON_WARNING);
+		case IMessageProvider.INFORMATION:
+			return display.getSystemImage(SWT.ICON_INFORMATION);
+		}
+		return null;
+	}
+
+	@Override
+	public Object getInput() {
+		return input;
+	}
+
+}
