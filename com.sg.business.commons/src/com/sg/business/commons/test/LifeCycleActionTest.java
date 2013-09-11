1efd87ef8454ff0e20a091c68fa540e15c82724c
ghuazh <ghuazh@gmail.com>
2013-09-12 00:04:33 +0800

    Signed-off-by: ghuazh <ghuazh@gmail.com>

--- com.sg.business.commons/src/com/sg/business/commons/test/LifeCycleActionTest.java
+++ com.sg.business.commons/src/com/sg/business/commons/test/LifeCycleActionTest.java
@@ -3,7 +3,6 @@ package com.sg.business.commons.test;
 import org.eclipse.core.expressions.PropertyTester;
 
 import com.sg.business.model.ILifecycle;
-import com.sg.widgets.part.CurrentAccountContext;
 
 public class LifeCycleActionTest extends PropertyTester {
 
