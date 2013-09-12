bdf64721821921abc7fa1ecd49f8eecf34cde26c
ghuazh <ghuazh@gmail.com>
2013-09-12 17:09:57 +0800

    Signed-off-by: ghuazh <ghuazh@gmail.com>

--- com.sg.business.project/src/com/sg/business/project/editor/page/WorkExecuteProcessAssignmentPage.java
+++ com.sg.business.project/src/com/sg/business/project/editor/page/WorkExecuteProcessAssignmentPage.java
@@ -5,9 +5,15 @@ import java.util.List;
 import org.eclipse.jface.viewers.ArrayContentProvider;
 import org.eclipse.jface.viewers.CellEditor;
 import org.eclipse.jface.viewers.ColumnLabelProvider;
+import org.eclipse.jface.viewers.ColumnViewerEditor;
+import org.eclipse.jface.viewers.ColumnViewerEditorActivationStrategy;
 import org.eclipse.jface.viewers.ComboBoxViewerCellEditor;
 import org.eclipse.jface.viewers.EditingSupport;
+import org.eclipse.jface.viewers.FocusCellOwnerDrawHighlighter;
+import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.viewers.TableViewerColumn;
+import org.eclipse.jface.viewers.TableViewerEditor;
+import org.eclipse.jface.viewers.TableViewerFocusCellManager;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 
@@ -19,6 +25,9 @@ import com.sg.business.model.IProcessControlable;
 import com.sg.business.model.User;
 import com.sg.business.model.Work;
 import com.sg.business.model.toolkit.UserToolkit;
+import com.sg.widgets.ImageResource;
+import com.sg.widgets.Widgets;
+import com.sg.widgets.commons.editingsupport.EditorActivationStrategy;
 import com.sg.widgets.part.editor.PrimaryObjectEditorInput;
 import com.sg.widgets.part.editor.page.AbstractFormPageDelegator;
 import com.sg.widgets.registry.config.BasicPageConfigurator;
@@ -40,7 +49,8 @@ public class WorkExecuteProcessAssignmentPage extends AbstractFormPageDelegator 
 			processActors = new BasicDBObject();
 		}
 
-		processViewer = new ProcessViewer(parent, Work.F_WF_EXECUTE, work,editable) {
+		processViewer = new ProcessViewer(parent, Work.F_WF_EXECUTE, work,
+				editable) {
 
 			@Override
 			protected void createColumns(ProcessViewer viewer) {
@@ -146,6 +156,18 @@ public class WorkExecuteProcessAssignmentPage extends AbstractFormPageDelegator 
 
 			};
 			column.setEditingSupport(editingSupport);
+			column.getColumn().setImage(Widgets.getImage(ImageResource.EDIT_12));
+
+			ColumnViewerEditorActivationStrategy activationStrategy = new EditorActivationStrategy(
+					viewer);
+			FocusCellOwnerDrawHighlighter highlighter = new FocusCellOwnerDrawHighlighter(
+					viewer);
+			int feature = ColumnViewerEditor.TABBING_HORIZONTAL
+					| ColumnViewerEditor.TABBING_MOVE_TO_ROW_NEIGHBOR;
+			TableViewerFocusCellManager focusManager = new TableViewerFocusCellManager(
+					(TableViewer) viewer, highlighter);
+			TableViewerEditor.create((TableViewer) viewer, focusManager,
+					activationStrategy, feature);
 		}
 		return column;
 
