22212c3fd4fc20e4ecfa3f30fc62d45fb7c282ca
yangjun1985 <gdiyang@lode>
2013-09-13 23:10:00 +0800

    Signed-off-by: yangjun1985 <gdiyang@lode>

--- com.sg.business.model/src/com/sg/business/model/User.java
+++ com.sg.business.model/src/com/sg/business/model/User.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.bson.types.ObjectId;
+import org.eclipse.swt.graphics.Image;
 
 import com.mobnut.db.DBActivator;
 import com.mobnut.db.model.IContext;
