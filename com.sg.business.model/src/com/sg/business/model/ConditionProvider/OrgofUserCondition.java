07d007d3d2d321c2754d23875f13e4b7afc9b6ce
ghuazh <ghuazh@gmail.com>
2013-09-17 14:54:14 +0800

    Signed-off-by: ghuazh <ghuazh@gmail.com>

--- com.sg.business.model/src/com/sg/business/model/ConditionProvider/OrgofUserCondition.java
+++ com.sg.business.model/src/com/sg/business/model/ConditionProvider/OrgofUserCondition.java
@@ -1,15 +1,20 @@
 package com.sg.business.model.ConditionProvider;
 
 import com.mobnut.db.model.IRelationConditionProvider;
+import com.mobnut.db.model.PrimaryObject;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DBObject;
+import com.sg.business.model.Organization;
 import com.sg.business.model.User;
 
 public class OrgofUserCondition implements IRelationConditionProvider {
 
 	@Override
-	public DBObject getCondition() {
-		return new BasicDBObject().append(mr.getEnd2Key(), getValue(mr.getEnd1Key())).append(User.F_ACTIVATED, Boolean.TRUE);
+	public DBObject getCondition(PrimaryObject po) {
+
+		return new BasicDBObject().append(User.F_ORGANIZATION_ID,
+				po.getValue(Organization.F__ID)).append(User.F_ACTIVATED,
+				Boolean.TRUE);
 	}
 
 }
