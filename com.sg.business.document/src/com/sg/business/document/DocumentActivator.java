68ceb4050de487f8ee1bb7de4938bc6876482e46
ghuazh <ghuazh@gmail.com>
2013-10-09 02:48:42 +0800

    添加文档插件，将在项目中的文档以及业务管理中的文档模板移动到本插件 为文档模板添加选定编辑器的支持
    Signed-off-by: ghuazh <ghuazh@gmail.com>

---
+++ com.sg.business.document/src/com/sg/business/document/DocumentActivator.java
@@ -0,0 +1,50 @@
+package com.sg.business.document;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class DocumentActivator extends AbstractUIPlugin {
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = "com.sg.business.document"; //$NON-NLS-1$
+
+	// The shared instance
+	private static DocumentActivator plugin;
+	
+	/**
+	 * The constructor
+	 */
+	public DocumentActivator() {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 *
+	 * @return the shared instance
+	 */
+	public static DocumentActivator getDefault() {
+		return plugin;
+	}
+
+}
