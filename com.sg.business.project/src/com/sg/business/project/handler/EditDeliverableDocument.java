6dee1785ff4f9de6f290569a1002575c91bfb571
yangjun1985 <gdiyang@lode>
2013-10-12 11:44:55 +0800

    1.工作无法编辑交付文档
    Signed-off-by: yangjun1985 <gdiyang@lode>

--- com.sg.business.project/src/com/sg/business/project/handler/EditDeliverableDocument.java
+++ com.sg.business.project/src/com/sg/business/project/handler/EditDeliverableDocument.java
@@ -10,18 +10,25 @@ import com.sg.widgets.MessageUtil;
 import com.sg.widgets.command.AbstractNavigatorHandler;
 import com.sg.widgets.part.editor.DataObjectEditor;
 
-public class ViewDeliverableDocument extends AbstractNavigatorHandler {
+public class EditDeliverableDocument extends AbstractNavigatorHandler {
 
 	@Override
 	protected void execute(PrimaryObject selected, ExecutionEvent event) {
 		if(selected instanceof Deliverable){
+			String editable = event.getParameter("deliverabledocument.editable"); //$NON-NLS-1$
+			Boolean _editable = null;
+			if("true".equalsIgnoreCase(editable)){
+				_editable = true;
+			}else if("false".equalsIgnoreCase(editable)){
+				_editable = false;
+			}
 			Deliverable deliverable = (Deliverable) selected;
 			Document doc = deliverable.getDocument();
 			if(doc==null){
 				MessageUtil.showToast("选择的交付物不包含文档", SWT.ICON_WARNING);
 			}else{
 				try {
-					DataObjectEditor.open(doc, doc.getDefaultEditorId(), false, null);
+					DataObjectEditor.open(doc, doc.getDefaultEditorId(), _editable, null);
 				} catch (Exception e) {
 					MessageUtil.showToast(e);
 				}
