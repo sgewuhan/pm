d75a096b0de30bb0dddb62e037629d4c507d4103
jinxitao <jxt@yaozheng.com.cn>
2013-10-16 09:30:49 +0800

    系统部通用审批流程TaskForm Signed-off-by: jinxitao <jxt@yaozheng.com.cn>

--- com.sg.business.model/src/com/sg/business/model/dataset/project/ProjectParticipate.java
+++ com.sg.business.model/src/com/sg/business/model/dataset/project/ProjectParticipate.java
@@ -8,6 +8,7 @@ import com.mobnut.db.model.PrimaryObject;
 import com.sg.business.model.IModelConstants;
 import com.sg.business.model.IProjectRelative;
 import com.sg.business.model.Project;
+import com.sg.business.model.TaskForm;
 import com.sg.business.model.User;
 import com.sg.business.model.toolkit.UserToolkit;
 import com.sg.widgets.commons.dataset.MasterDetailDataSetFactory;
@@ -25,10 +26,37 @@ public class ProjectParticipate extends MasterDetailDataSetFactory {
 
 	@Override
 	public DataSet getDataSet() {
+//		if (master != null) {
+//			IProjectRelative work = (IProjectRelative) master;
+//			Project project = work.getProject();
+//			List<?> participates = project.getParticipatesIdList();
+//			List<PrimaryObject> result = new ArrayList<PrimaryObject>();
+//			if (participates != null) {
+//				for (int i = 0; i < participates.size(); i++) {
+//					String userid = (String) participates.get(i);
+//					User user = UserToolkit.getUserById(userid);
+//					result.add(user);
+//				}
+//			}
+//			return new DataSet(result);
+//		} else {
+//			return super.getDataSet();
+//		}
+		
+		
 		if (master != null) {
-			IProjectRelative work = (IProjectRelative) master;
-			Project project = work.getProject();
-			List<?> participates = project.getParticipatesIdList();
+			Project project=null;
+			if(master instanceof TaskForm){
+				TaskForm taskForm=(TaskForm)master;
+				project=taskForm.getWork().getProject();
+			}else{
+				IProjectRelative work = (IProjectRelative) master;
+	  			project = work.getProject();
+			}
+			List<?> participates=null;
+			if(project!=null){
+				 participates = project.getParticipatesIdList();
+			}
 			List<PrimaryObject> result = new ArrayList<PrimaryObject>();
 			if (participates != null) {
 				for (int i = 0; i < participates.size(); i++) {
