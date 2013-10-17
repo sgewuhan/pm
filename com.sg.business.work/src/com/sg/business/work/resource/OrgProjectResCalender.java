dd020591dddf1aef3f359c2768cf13ec67a79159
ghuazh <ghuazh@gmail.com>
2013-10-17 15:42:03 +0800

    Signed-off-by: ghuazh <ghuazh@gmail.com>

---
+++ com.sg.business.work/src/com/sg/business/work/resource/OrgProjectResCalender.java
@@ -0,0 +1,24 @@
+package com.sg.business.work.resource;
+
+import java.util.List;
+
+import org.eclipse.jface.viewers.IContentProvider;
+
+import com.mobnut.db.model.PrimaryObject;
+
+public class OrgProjectResCalender extends ResourceCalender {
+
+	@Override
+	protected IContentProvider getContentProvider() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected List<PrimaryObject> getInput() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+}
