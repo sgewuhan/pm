f88f9e73941dccaafbcb943ab0234ab2c7b5e522
ghuazh <ghuazh@gmail.com>
2013-10-21 16:39:36 +0800

    Signed-off-by: ghuazh <ghuazh@gmail.com>

---
+++ com.sg.business.performence/src/com/sg/business/performence/ui/handler/AppendWorkRecord.java
@@ -0,0 +1,22 @@
+package com.sg.business.performence.ui.handler;
+
+import java.util.Map;
+
+import org.eclipse.core.commands.Command;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IWorkbenchPart;
+
+import com.mobnut.db.model.PrimaryObject;
+import com.sg.widgets.command.AbstractNavigatorHandler;
+import com.sg.widgets.viewer.ViewerControl;
+
+public class AppendWorkRecord extends AbstractNavigatorHandler {
+
+	@Override
+	protected void execute(PrimaryObject selected, IWorkbenchPart part,
+			ViewerControl vc, Command command, Map<String, Object> parameters,
+			IStructuredSelection selection) {
+
+	}
+
+}
