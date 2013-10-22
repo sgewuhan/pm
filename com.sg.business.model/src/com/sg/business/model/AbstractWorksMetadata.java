751a00af34c496fe0634a23cfc3fdb414c3188f5
ghuazh <ghuazh@gmail.com>
2013-10-22 13:11:02 +0800

    Signed-off-by: ghuazh <ghuazh@gmail.com>

--- com.sg.business.model/src/com/sg/business/model/AbstractWorksMetadata.java
+++ com.sg.business.model/src/com/sg/business/model/AbstractWorksMetadata.java
@@ -1,5 +1,7 @@
 package com.sg.business.model;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 import org.bson.types.ObjectId;
@@ -8,7 +10,8 @@ import com.mobnut.db.model.IContext;
 import com.mobnut.db.model.ModelService;
 import com.mobnut.db.model.PrimaryObject;
 
-public abstract class AbstractWorksMetadata extends PrimaryObject implements IProjectRelative{
+public abstract class AbstractWorksMetadata extends PrimaryObject implements
+		IProjectRelative {
 
 	public static final String F_WORKID = "workid";
 	public static final String F_USERID = "userid";
@@ -18,72 +21,112 @@ public abstract class AbstractWorksMetadata extends PrimaryObject implements IPr
 	public static final String F_PROJECTDESC = "projectdesc";
 	public static final String F_WORKDESC = "workdesc";
 	public static final String F_PLANWORKS = "planworks";
-	
+
 	@Override
 	public boolean doSave(IContext context) throws Exception {
-		
+
 		Object value = getValue(F_WORKS);
-		if(!(value instanceof Double)){
+		if (!(value instanceof Double)) {
 			throw new Exception("缺少实际工时数据");
 		}
-		
+
 		value = getValue(F_USERID);
-		if(!(value instanceof String)){
+		if (!(value instanceof String)) {
 			throw new Exception("缺少执行人");
 		}
-		
+
 		Object workid = getValue(F_WORKID);
-		if(!(workid instanceof ObjectId)){
+		if (!(workid instanceof ObjectId)) {
 			throw new Exception("缺少目标工作");
 		}
-		
-		Work work = ModelService.createModelObject(Work.class, (ObjectId)workid);
+
+		Work work = ModelService.createModelObject(Work.class,
+				(ObjectId) workid);
 		Project project = work.getProject();
-		
+
+		String ls = work.getLifecycleStatus();
+		if (!Work.STATUS_WIP_VALUE.equals(ls)) {
+			throw new Exception("只能为进行中的工作提交工时记录");
+		}
+
+		value = getValue(F_DATECODE);
+		Date dateValue = null;
+		if (value instanceof Date) {
+			dateValue = (Date) value;
+			if (dateValue.after(new Date())) {
+				throw new Exception("提交工时日期不能晚于当前日期");
+			}
+
+		} else if (!(value instanceof Long)) {
+			dateValue = new Date();
+		}else{
+			Calendar cal = Calendar.getInstance();
+			cal.setTimeInMillis(((Long)value).longValue()*24*60*60*1000);
+			dateValue = cal.getTime();
+		}
+
+		Date as = work.getActualStart();
+		if (dateValue.before(as)) {
+			throw new Exception("提交工时日期不能早于工作的实际开始日期");
+		}
+
+		Long dateCode = new Long(dateValue.getTime() / (24 * 60 * 60 * 1000));
+		setValue(F_DATECODE, dateCode);
+
 		value = getValue(F_WORKDESC);
-		if(value == null){
+		if (value == null) {
 			setValue(F_WORKDESC, work.getLabel());
 		}
-		
+
 		value = getValue(F_PLANWORKS);
-		if(value == null){
+		if (value == null) {
 			setValue(F_PLANWORKS, work.getPlanWorks());
 		}
-		
+
 		value = getValue(F_PROJECT_ID);
-		if(value == null){
+		if (value == null) {
 			setValue(F_PROJECT_ID, project.get_id());
 		}
-		
+
 		value = getValue(F_PROJECTDESC);
-		if(value == null){
-			setValue(F_PROJECTDESC,project.getLabel());
+		if (value == null) {
+			setValue(F_PROJECTDESC, project.getLabel());
 		}
-		
-		value = getValue(F_DATECODE);
-		if(value instanceof Date){
-			Long dateCode = new Long(((Date)value).getTime()/(24*60*60*1000));
-			setValue(F_DATECODE, dateCode);
-		}else if(!(value instanceof Long)){
-			Date date = new Date();
-			Long dateCode = new Long(date.getTime()/(24*60*60*1000));
-			setValue(F_DATECODE, dateCode);
-		}
-		
-		
-		
-		
-		
+
 		return super.doSave(context);
 	}
-	
+
 	@Override
 	public Project getProject() {
 		Object projectId = getValue(F_PROJECT_ID);
-		if(projectId instanceof ObjectId){
-			return ModelService.createModelObject(Project.class, (ObjectId)projectId);
+		if (projectId instanceof ObjectId) {
+			return ModelService.createModelObject(Project.class,
+					(ObjectId) projectId);
 		}
 		return null;
 	}
-	
+
+	@Override
+	public String getLabel() {
+		Object value = getValue(F_DATECODE);
+		if (!(value instanceof Long)) {
+			return "";
+		}
+		long millis = ((Long) value).longValue() * 24 * 60 * 60 * 1000;
+		Calendar cal = Calendar.getInstance();
+		cal.setTimeInMillis(millis);
+		Date date = cal.getTime();
+		return new SimpleDateFormat("yyyy-MM-dd").format(date);
+	}
+
+	public Work getWork() {
+		Object workid = getValue(F_WORKID);
+		if (workid instanceof ObjectId) {
+			Work work = ModelService.createModelObject(Work.class,
+					(ObjectId) workid);
+			return work;
+		}
+		return null;
+	}
+
 }
--- com.sg.business.model/src/com/sg/business/model/WorksPerformence.java
+++ com.sg.business.model/src/com/sg/business/model/WorksPerformence.java
@@ -3,9 +3,16 @@ package com.sg.business.model;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Iterator;
 
+import org.bson.types.ObjectId;
 import org.eclipse.swt.graphics.Image;
 
+import com.mobnut.db.model.IContext;
+import com.mongodb.AggregationOutput;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
 import com.sg.business.resource.BusinessResource;
 
 public class WorksPerformence extends AbstractWorksMetadata {
@@ -14,22 +21,55 @@ public class WorksPerformence extends AbstractWorksMetadata {
 	public Image getImage() {
 		return BusinessResource.getImage(BusinessResource.IMAGE_LOG_16);
 	}
-	
-//	@Override
-//	public String getLabel() {
-//		return getDesc()+" "+getLogDate();
-//		
-//	}
+
+	// @Override
+	// public String getLabel() {
+	// return getDesc()+" "+getLogDate();
+	//
+	// }
 
 	public String getLogDate() {
 		Long dateCode = (Long) getValue(F_DATECODE);
-		if(dateCode!=null){
+		if (dateCode != null) {
 			Calendar cal = Calendar.getInstance();
-			cal.setTimeInMillis(dateCode.longValue()*24*60*60*1000);
+			cal.setTimeInMillis(dateCode.longValue() * 24 * 60 * 60 * 1000);
 			Date date = cal.getTime();
 			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 			return sdf.format(date);
 		}
 		return null;
 	}
+
+	@Override
+	public boolean doSave(IContext context) throws Exception {
+		super.doSave(context);
+
+		// 合计工作的实际工时
+		ObjectId workId = (ObjectId) getValue(F_WORKID);
+
+		DBObject match = new BasicDBObject();
+		match.put("$match", new BasicDBObject().append(F_WORKID, workId));
+
+		DBObject group = new BasicDBObject();
+		group.put(
+				"$group",
+				new BasicDBObject().append("_id", "$" + F_WORKID).append(
+						F_WORKS,
+						new BasicDBObject().append("$sum", "$" + F_WORKS)));
+		DBCollection col = getCollection();
+		AggregationOutput result = col.aggregate(match, group);
+		Iterator<DBObject> iter = result.results().iterator();
+		if (iter.hasNext()) {
+			DBObject data = iter.next();
+			Object summary = data.get(F_WORKS);
+			if (summary instanceof Double) {
+				col = getCollection(IModelConstants.C_WORK);
+				col.update(new BasicDBObject().append(Work.F__ID, workId),
+						new BasicDBObject().append("$set", new BasicDBObject()
+								.append(Work.F_ACTUAL_WORKS, summary)));
+			}
+		}
+		return true;
+	}
+
 }
