6457937232383151f700f2bf04039c965b2cea4c
yangjun1985 <yj@yaozheng.com.cn>
2013-10-26 11:52:57 +0800

    Signed-off-by: yangjun1985 <yj@yaozheng.com.cn>

---
+++ com.tmt.kh/src/com/tmt/kh/bpmservice/ApproveMessageOfKH.java
@@ -0,0 +1,11 @@
+package com.tmt.kh.bpmservice;
+
+import com.sg.business.model.bpmservice.MessageService;
+
+public class ApproveMessageOfKH extends MessageService {
+
+	public ApproveMessageOfKH() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
