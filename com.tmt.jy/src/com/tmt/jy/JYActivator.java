f39c8ed4c5be69b87ea5d5d36894aa21efedaf0f
ghuazh <ghuazh@gmail.com>
2013-10-29 23:50:30 +0800

    Signed-off-by: ghuazh <ghuazh@gmail.com>

---
+++ com.tmt.jy/src/com/tmt/jy/JYActivator.java
@@ -0,0 +1,50 @@
+package com.tmt.jy;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class JYActivator extends AbstractUIPlugin {
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = "com.tmt.jy"; //$NON-NLS-1$
+
+	// The shared instance
+	private static JYActivator plugin;
+	
+	/**
+	 * The constructor
+	 */
+	public JYActivator() {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 *
+	 * @return the shared instance
+	 */
+	public static JYActivator getDefault() {
+		return plugin;
+	}
+
+}
