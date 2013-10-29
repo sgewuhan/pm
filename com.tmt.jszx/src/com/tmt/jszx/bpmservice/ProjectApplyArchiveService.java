a3e479681c398136221dcbcc3d7dd7e397ebb17d
yangjun1985 <yj@yaozheng.com.cn>
2013-10-29 13:15:58 +0800

    技术中心委外流程-文件归档服务
    Signed-off-by: yangjun1985 <yj@yaozheng.com.cn>

--- com.tmt.jszx/src/com/tmt/jszx/bpmservice/ProjectApplyArchiveService.java
+++ com.tmt.jszx/src/com/tmt/jszx/bpmservice/ProjectApplyArchiveService.java
@@ -1,6 +1,7 @@
 package com.tmt.jszx.bpmservice;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.bson.types.BasicBSONList;
@@ -8,13 +9,19 @@ import org.bson.types.ObjectId;
 
 import com.mobnut.db.model.ModelService;
 import com.mobnut.db.model.PrimaryObject;
+import com.mongodb.BasicDBObject;
 import com.mongodb.DBObject;
 import com.sg.bpm.service.task.ServiceProvider;
 import com.sg.bpm.workflow.utils.WorkflowUtils;
+import com.sg.business.model.Container;
+import com.sg.business.model.Document;
+import com.sg.business.model.Folder;
+import com.sg.business.model.IModelConstants;
 import com.sg.business.model.IProcessControl;
 import com.sg.business.model.IWorkCloneFields;
 import com.sg.business.model.Organization;
 import com.sg.business.model.Work;
+import com.sg.business.model.bpmservice.BPMServiceContext;
 
 public class ProjectApplyArchiveService extends ServiceProvider {
 
@@ -29,6 +36,8 @@ public class ProjectApplyArchiveService extends ServiceProvider {
 			String jsonContent = (String) content;
 			try {
 				PrimaryObject host = WorkflowUtils.getHostFromJSON(jsonContent);
+				String processId = (String) host.getValue("processId");
+				String processName = (String) host.getValue("processName");
 				if (host instanceof Work) {
 					Work work = (Work) host;
 					ObjectId orgId = null;
@@ -56,6 +65,42 @@ public class ProjectApplyArchiveService extends ServiceProvider {
 										+ work.getProject().getDesc();
 								filderNmae = filderNmae + "-" + work.getDesc();
 								ObjectId folderId;
+								BasicDBObject folderCondition = new BasicDBObject();
+								folderCondition.put(Folder.F_IS_WORKFLOW_FOLDER, Boolean.FALSE);
+								folderCondition.put(Folder.F_DESC, filderNmae);
+								folderCondition.put(Folder.F_IS_PROJECT_FOLDERROOT, Boolean.FALSE);
+								folderCondition.put(Folder.F_ROOT_ID, containerOrgId);
+								String containerCollection, containerDB;
+								containerCollection = IModelConstants.C_ORGANIZATION;
+								Container container = Container.adapter(work.getProject(),
+										Container.TYPE_ADMIN_GRANTED);
+								containerDB = (String) container
+										.getValue(Container.F_SOURCE_DB);
+								folderCondition.put(Folder.F_CONTAINER_DB, containerDB);
+								folderCondition.put(Folder.F_CONTAINER_COLLECTION,
+										containerCollection);
+								Folder wfFolder = ModelService.createModelObject(folderCondition,
+										Folder.class);
+								wfFolder.doInsert(new BPMServiceContext(processName, processId));
+								folderId = wfFolder.get_id();
+								
+								List<PrimaryObject> documentList = work.getDeliverableDocuments();
+								for (PrimaryObject po : documentList) {
+									if(po instanceof Document){
+										Document document = (Document) po;
+										BasicDBObject newDocument = new BasicDBObject();
+										newDocument.put(Document.F__SUMMARY, document.getValue(Document.F__SUMMARY));
+										newDocument.put(Document.F_DESCRIPTION, document.getValue(Document.F_DESCRIPTION));
+										newDocument.put(Document.F_DOCUMENT_NUMBER, document.getValue(Document.F_DOCUMENT_NUMBER));
+										newDocument.put(Document.F_DOCUMENT_TYPE, document.getValue(Document.F_DOCUMENT_TYPE));
+										newDocument.put(Document.F_FOLDER_ID, folderId);
+										newDocument.put(Document.F_VAULT, document.getValue(Document.F_VAULT));
+										newDocument.put(Document.F_WORK_ID, document.getValue(Document.F_WORK_ID));
+										newDocument.put(Document.F_PROJECT_ID, document.getValue(Document.F_PROJECT_ID));
+										Document newdocument = ModelService.createModelObject(newDocument, Document.class);
+										newdocument.doSave(new BPMServiceContext(processName, processId));
+									}
+								}
 								
 							} else {
 								result.put("returnCode", "ERROR");
