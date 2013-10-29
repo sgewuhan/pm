c1ba6be2bcd3733e915748493417185bc8619f05
yangjun1985 <yj@yaozheng.com.cn>
2013-10-29 19:16:41 +0800

    Signed-off-by: yangjun1985 <yj@yaozheng.com.cn>

---
+++ com.tmt.kfzx/src/com/tmt/kfzx/bpmservice/ApproveMessageOfKFZX.java
@@ -0,0 +1,10 @@
+package com.tmt.kfzx.bpmservice;
+
+import com.sg.business.model.bpmservice.MessageService;
+
+public class ApproveMessageOfKFZX extends MessageService {
+
+	public ApproveMessageOfKFZX() {
+	}
+
+}
