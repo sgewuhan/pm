13dfcc49c9191afcc054f1aeb4fb8aefc9ba34fe
jinxitao <jxt@yaozheng.com.cn>
2013-10-31 13:36:31 +0800

    Signed-off-by: jinxitao <jxt@yaozheng.com.cn>

--- com.tmt.kfzx/src/com/tmt/kfzx/field/parameter/DevelopmentDirector.java
+++ com.tmt.kfzx/src/com/tmt/kfzx/field/parameter/DevelopmentDirector.java
@@ -1,34 +1,14 @@
 package com.tmt.kfzx.field.parameter;
 
-import java.util.List;
-
-import org.bson.types.ObjectId;
-
-import com.mobnut.db.model.ModelService;
-import com.mobnut.db.model.PrimaryObject;
-import com.sg.bpm.workflow.taskform.IProcessParameterDelegator;
-import com.sg.business.model.Organization;
-import com.sg.business.model.User;
-import com.sg.business.model.toolkit.UserToolkit;
+import com.sg.business.taskforms.AbstractRoleParameterDelegator;
 import com.sg.business.taskforms.IRoleConstance;
 
-public class DevelopmentDirector implements IProcessParameterDelegator {
+public class DevelopmentDirector extends AbstractRoleParameterDelegator {
 
 
 	@Override
-	public Object getValue(String processParameter, String taskDatakey,
-			PrimaryObject taskFormData) {
-		Object value = taskFormData.getValue(taskDatakey);
-		if (value instanceof ObjectId) {
-			Organization org = ModelService.createModelObject(
-					Organization.class, (ObjectId) value);
-			List<String> users = org.getRoleAssignmentUserIds(
-					IRoleConstance.ROLE_DEVELOPMENTDIRECTOR_ID, 1);
-			if(!users.isEmpty()){
-				return users.get(0);
-			}
-		}
-		return ((User) UserToolkit.getAdmin().get(0)).getUserid();
+	protected String getRoldNumber() {
+		return IRoleConstance.ROLE_DEVELOPMENTDIRECTOR_ID;
 	}
 
 }
