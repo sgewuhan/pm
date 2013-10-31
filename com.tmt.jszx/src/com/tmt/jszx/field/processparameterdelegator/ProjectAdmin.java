5b67e426b6ba0fc7a2eb2ed468f3558f83aae7e3
jinxitao <jxt@yaozheng.com.cn>
2013-10-31 13:20:38 +0800

    Signed-off-by: jinxitao <jxt@yaozheng.com.cn>

--- com.tmt.jszx/src/com/tmt/jszx/field/processparameterdelegator/ProjectAdmin.java
+++ com.tmt.jszx/src/com/tmt/jszx/field/processparameterdelegator/ProjectAdmin.java
@@ -1,39 +1,16 @@
 package com.tmt.jszx.field.processparameterdelegator;
 
-import java.util.List;
-
-import org.bson.types.ObjectId;
-
-import com.mobnut.db.model.ModelService;
-import com.mobnut.db.model.PrimaryObject;
-import com.sg.bpm.workflow.taskform.IProcessParameterDelegator;
-import com.sg.business.model.Organization;
 import com.sg.business.model.Role;
-import com.sg.business.model.RoleAssignment;
-import com.sg.business.model.User;
-import com.sg.business.model.toolkit.UserToolkit;
+import com.sg.business.taskforms.AbstractRoleParameterDelegator;
 
-public class ProjectAdmin implements IProcessParameterDelegator {
+public class ProjectAdmin  extends AbstractRoleParameterDelegator {
 
 	public ProjectAdmin() {
 	}
 
 	@Override
-	public Object getValue(String processParameter, String taskDatakey,
-			PrimaryObject taskFormData) {
-
-		Object value = taskFormData.getValue(taskDatakey);
-		if(value instanceof ObjectId){
-			Organization org = ModelService.createModelObject(Organization.class, (ObjectId)value);
-			Role role = org.getRole(Role.ROLE_PROJECT_ADMIN_ID, 1);
-			if (role != null) {
-				List<PrimaryObject> assignment = role.getAssignment();
-				if (assignment != null && assignment.size() > 0) {
-					return ((RoleAssignment) assignment.get(0)).getUserid();
-				}
-			}
-		}
-		return ((User) UserToolkit.getAdmin().get(0)).getUserid();
+	protected String getRoldNumber() {
+		return Role.ROLE_PROJECT_ADMIN_ID;
 	}
 
 }
