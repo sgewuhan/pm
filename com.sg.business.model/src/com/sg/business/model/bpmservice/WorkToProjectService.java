a0c5fc418040f285447c2547e4561689dde96a58
jinxitao <jxt@yaozheng.com.cn>
2013-10-31 09:11:31 +0800

    Signed-off-by: jinxitao <jxt@yaozheng.com.cn>

---
+++ com.sg.business.model/src/com/sg/business/model/bpmservice/WorkToProjectService.java
@@ -0,0 +1,18 @@
+package com.sg.business.model.bpmservice;
+
+import java.util.Map;
+
+import com.sg.bpm.service.task.ServiceProvider;
+
+public class WorkToProjectService extends ServiceProvider {
+
+	public WorkToProjectService() {
+	}
+
+	@Override
+	public Map<String, Object> run(Object parameter) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
