69c1e47576c02ecb39f583d444f6bafc08fd850c
jinxitao <jxt@yaozheng.com.cn>
2013-11-01 09:59:47 +0800

    Signed-off-by: jinxitao <jxt@yaozheng.com.cn>

--- com.tmt.jszx/src/com/tmt/jszx/actor/ChiefEngineerOfLauncher.java
+++ com.tmt.jszx/src/com/tmt/jszx/actor/ChiefEngineerOfLauncher.java
@@ -1,36 +1,13 @@
 package com.tmt.jszx.actor;
 
-import java.util.List;
-
-import com.mobnut.db.model.PrimaryObject;
-import com.sg.bpm.service.actor.IActorIdProvider;
-import com.sg.business.model.Organization;
-import com.sg.business.model.Role;
-import com.sg.business.model.RoleAssignment;
-import com.sg.business.model.User;
-import com.sg.business.model.Work;
-import com.sg.business.model.toolkit.UserToolkit;
+import com.sg.business.taskforms.AbstractRoleParameterDelegator;
 import com.sg.business.taskforms.IRoleConstance;
 
-public class ChiefEngineerOfLauncher implements IActorIdProvider {
-
-	public ChiefEngineerOfLauncher() {
-	}
+public class ChiefEngineerOfLauncher extends AbstractRoleParameterDelegator {
 
 	@Override
-	public String getActorId(Object[] input) {
-		Work work = (Work) input[0];
-		String chargerId = work.getChargerId();
-		User loginUser = UserToolkit.getUserById(chargerId);
-		Organization org = loginUser.getOrganization();
-		Role role = org.getRole(IRoleConstance.ROLE_CHIEF_ENGINEER_ID, 1);
-		if (role != null) {
-			List<PrimaryObject> assignment = role.getAssignment();
-			if (assignment != null && assignment.size() > 0) {
-				return ((RoleAssignment) assignment.get(0)).getUserid();
-			}
-		}
-		return ((User) UserToolkit.getAdmin().get(0)).getUserid();
+	protected String getRoldNumber() {
+		return IRoleConstance.ROLE_CHIEF_ENGINEER_ID;
 	}
 
 }
