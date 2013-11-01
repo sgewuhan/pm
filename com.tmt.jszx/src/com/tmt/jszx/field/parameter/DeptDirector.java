75cea00b7c282af1adafe5da045135e06fd7af27
jinxitao <jxt@yaozheng.com.cn>
2013-11-01 18:01:54 +0800

    Signed-off-by: jinxitao <jxt@yaozheng.com.cn>

--- com.tmt.jszx/src/com/tmt/jszx/field/parameter/DeptDirector.java
+++ com.tmt.jszx/src/com/tmt/jszx/field/parameter/DeptDirector.java
@@ -1,14 +1,14 @@
 package com.tmt.jszx.field.parameter;
 
+import com.sg.business.model.Role;
 import com.sg.business.taskforms.AbstractRoleParameterDelegator;
-import com.sg.business.taskforms.IRoleConstance;
 
 public class DeptDirector extends AbstractRoleParameterDelegator {
 
 
 	@Override
 	protected String getRoldNumber() {
-		return IRoleConstance.ROLE_DIRECTOR_ID;
+		return Role.ROLE_DEPT_MANAGER_ID;
 	}
 
 }
