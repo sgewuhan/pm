dd40c5c1d158606abb14c4308c8d9293dcc8640e
yangjun1985 <yj@yaozheng.com.cn>
2013-11-05 20:11:42 +0800

    Signed-off-by: yangjun1985 <yj@yaozheng.com.cn>

--- com.tmt.jszx/src/com/tmt/jszx/handler/ProjectReviewConvenerOfJSZX.java
+++ com.tmt.jszx/src/com/tmt/jszx/handler/ProjectReviewConvenerOfJSZX.java
@@ -1,52 +1,44 @@
 package com.tmt.jszx.handler;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import org.bson.types.BasicBSONList;
 import org.jbpm.task.Task;
 
 import com.mobnut.db.model.PrimaryObject;
-import com.mongodb.DBObject;
 import com.sg.bpm.workflow.taskform.ITaskFormInputHandler;
 import com.sg.bpm.workflow.taskform.TaskFormConfig;
-import com.sg.business.model.IProcessControl;
-import com.sg.business.model.IWorkCloneFields;
 import com.sg.business.model.TaskForm;
-import com.sg.business.model.Work;
+import com.sg.widgets.part.CurrentAccountContext;
 
 public class ProjectReviewConvenerOfJSZX implements ITaskFormInputHandler {
 
 	public ProjectReviewConvenerOfJSZX() {
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public PrimaryObject getTaskFormInputData(PrimaryObject taskFormData,
 			TaskFormConfig taskFormConfig, Task task) {
 		if (taskFormData instanceof TaskForm) {
 			TaskForm taskForm = (TaskForm) taskFormData;
-			Work work = taskForm.getWork();
-			IProcessControl ip = work.getAdapter(IProcessControl.class);
-			BasicBSONList historys = ip.getWorkflowHistroyData(
-					IWorkCloneFields.F_WF_EXECUTE, true);
-			for (int i = 0; i < historys.size(); i++) {
-				DBObject history = (DBObject) historys.get(i);
-				String taskname = (String) history
-						.get(IProcessControl.F_WF_TASK_NAME);
-				if ("提交评审".equals(taskname)) {
-					List<?> form_reviewer_list = (List<?>) history
-							.get("form_reviewer_list");
-					if (form_reviewer_list != null) {
-						taskForm.setValue("reviewer_list",
-								(Object) form_reviewer_list);
-					}
-				} else if ("批准评审".equals(taskname)) {
-					String form_review_convener = (String) history
-							.get("form_review_convener");
-					if (form_review_convener != null) {
-						taskForm.setValue("review_convener",
-								(Object) form_review_convener);
-					}
+			try {
+				List<String> reviewerList = (ArrayList<String>) taskForm
+						.getProcessInstanceVarible("reviewer_list",
+								new CurrentAccountContext());
+				if (reviewerList != null && reviewerList.size() > 0) {
+					taskForm.setValue("reviewer_list", (Object) reviewerList);
 				}
+				String reviewConvener = (String) taskForm
+						.getProcessInstanceVarible("review_convener",
+								new CurrentAccountContext());
+				if (reviewerList != null && reviewerList.size() > 0) {
+					taskForm.setValue("review_convener",
+							(Object) reviewConvener);
+				}
+
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
 			return taskForm;
 		}
