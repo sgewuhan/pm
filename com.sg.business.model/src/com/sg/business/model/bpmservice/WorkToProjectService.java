5ad591f2207444339c5bf727dc5f4d2f76b408f1
jinxitao <jxt@yaozheng.com.cn>
2013-11-05 20:26:15 +0800

    Signed-off-by: jinxitao <jxt@yaozheng.com.cn>

--- com.sg.business.model/src/com/sg/business/model/bpmservice/WorkToProjectService.java
+++ com.sg.business.model/src/com/sg/business/model/bpmservice/WorkToProjectService.java
@@ -1,10 +1,8 @@
 package com.sg.business.model.bpmservice;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-import org.bson.types.ObjectId;
 import com.mobnut.db.DBActivator;
 import com.mobnut.db.model.ModelService;
 import com.mobnut.db.model.PrimaryObject;
@@ -13,12 +11,10 @@ import com.mongodb.DBCollection;
 import com.mongodb.DBObject;
 import com.sg.bpm.service.task.ServiceProvider;
 import com.sg.bpm.workflow.utils.WorkflowUtils;
-import com.sg.business.model.Deliverable;
-import com.sg.business.model.Document;
-import com.sg.business.model.Folder;
 import com.sg.business.model.IModelConstants;
 import com.sg.business.model.Project;
 import com.sg.business.model.Work;
+import com.sg.business.model.toolkit.ProjectToolkit;
 
 public class WorkToProjectService extends ServiceProvider {
 
@@ -38,8 +34,11 @@ public class WorkToProjectService extends ServiceProvider {
 				if (host instanceof Work) {
 					Work work = (Work) host;
 					// 根据工作令号获取项目
-					Project project = null;
-					String workorder = (String) getInputValue("number");
+					Project project = work.getProject();
+					if(project!=null){
+						return result;
+					}
+					String workorder = (String) getInputValue("workorder");
 					DBCollection col = DBActivator.getCollection(
 							IModelConstants.DB, IModelConstants.C_PROJECT);
 					DBObject dbo = col.findOne(new BasicDBObject().append(
@@ -50,33 +49,34 @@ public class WorkToProjectService extends ServiceProvider {
 					}
 					// 附加工作至项目中
 					if (project != null) {
-						ObjectId project_id = project.get_id();
-						Folder folderRoot = project.getFolderRoot();
-						Work rootwork = project.getWBSRoot();
-						int maxChildSeq = rootwork.getMaxChildSeq();
-						work.setValue(Work.F_PARENT_ID, rootwork.get_id());
-						work.setValue(Work.F_PROJECT_ID, project_id);
-						work.setValue(Work.F_SEQ, maxChildSeq+1);
-						work.doSave((new BPMServiceContext(processName,
-								processId)));
-
-						// 文档归档
-						List<PrimaryObject> deliverable = work.getDeliverable();
-						for (PrimaryObject del : deliverable) {
-							del.setValue(Deliverable.F_PROJECT_ID, project_id);
-							List<PrimaryObject> deliverableDocuments = work
-									.getDeliverableDocuments();
-							for (PrimaryObject doc : deliverableDocuments) {
-								doc.setValue(Document.F_PROJECT_ID, project_id);
-								doc.setValue(Document.F_FOLDER_ID,
-										folderRoot.get_id());
-								doc.doSave((new BPMServiceContext(processName,
-										processId)));
-							}
-							del.doSave((new BPMServiceContext(processName,
-									processId)));
-
-						}
+						ProjectToolkit.doProjectAddStandloneWork(work,project,(new BPMServiceContext(processName,processId)));
+//						ObjectId project_id = project.get_id();
+//						Folder folderRoot = project.getFolderRoot();
+//						Work rootwork = project.getWBSRoot();
+//						int maxChildSeq = rootwork.getMaxChildSeq();
+//						work.setValue(Work.F_PARENT_ID, rootwork.get_id());
+//						work.setValue(Work.F_PROJECT_ID, project_id);
+//						work.setValue(Work.F_SEQ, maxChildSeq+1);
+//						work.doSave((new BPMServiceContext(processName,
+//								processId)));
+//
+//						// 文档归档
+//						List<PrimaryObject> deliverable = work.getDeliverable();
+//						for (PrimaryObject del : deliverable) {
+//							del.setValue(Deliverable.F_PROJECT_ID, project_id);
+//							List<PrimaryObject> deliverableDocuments = work
+//									.getDeliverableDocuments();
+//							for (PrimaryObject doc : deliverableDocuments) {
+//								doc.setValue(Document.F_PROJECT_ID, project_id);
+//								doc.setValue(Document.F_FOLDER_ID,
+//										folderRoot.get_id());
+//								doc.doSave((new BPMServiceContext(processName,
+//										processId)));
+//							}
+//							del.doSave((new BPMServiceContext(processName,
+//									processId)));
+//
+//						}
 					}
 
 				} else {
