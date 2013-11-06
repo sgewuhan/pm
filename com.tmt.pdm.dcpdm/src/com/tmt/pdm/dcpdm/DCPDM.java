85f1e9815c0af2753858f2986245c8e896ef3735
ghuazh <ghuazh@gmail.com>
2013-11-06 09:59:34 +0800

    Signed-off-by: ghuazh <ghuazh@gmail.com>

---
+++ com.tmt.pdm.dcpdm/src/com/tmt/pdm/dcpdm/DCPDM.java
@@ -0,0 +1,18 @@
+package com.tmt.pdm.dcpdm;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class DCPDM implements BundleActivator {
+
+	@Override
+	public void start(BundleContext context) throws Exception {
+
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+
+	}
+
+}
