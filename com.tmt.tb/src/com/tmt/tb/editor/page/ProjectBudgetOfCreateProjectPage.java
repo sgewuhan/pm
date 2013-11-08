4b3e3d6bfabbd255dfe206239f844f80bf869565
yangjun1985 <yj@yaozheng.com.cn>
2013-11-08 09:43:20 +0800

    Signed-off-by: yangjun1985 <yj@yaozheng.com.cn>

--- com.tmt.tb/src/com/tmt/tb/editor/page/ProjectBudgetOfCreateProjectPage.java
+++ com.tmt.tb/src/com/tmt/tb/editor/page/ProjectBudgetOfCreateProjectPage.java
@@ -1,7 +1,9 @@
 package com.tmt.tb.editor.page;
 
 import java.text.DecimalFormat;
+import java.util.List;
 
+import org.bson.types.ObjectId;
 import org.eclipse.jface.viewers.CellEditor;
 import org.eclipse.jface.viewers.ColumnLabelProvider;
 import org.eclipse.jface.viewers.EditingSupport;
@@ -14,31 +16,41 @@ import org.eclipse.ui.forms.IFormPart;
 import org.eclipse.ui.forms.IManagedForm;
 
 import com.mobnut.commons.util.Utils;
+import com.mobnut.db.model.IPrimaryObjectValueChangeListener;
+import com.mobnut.db.model.ModelService;
+import com.mobnut.db.model.PrimaryObject;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
 import com.sg.business.commons.page.ProjectBudgetTreeContentProvider;
 import com.sg.business.model.Project;
 import com.sg.business.model.ProjectBudget;
+import com.sg.business.model.ProjectTemplate;
+import com.sg.business.model.TaskForm;
 import com.sg.widgets.part.CurrentAccountContext;
 import com.sg.widgets.part.editor.PrimaryObjectEditorInput;
 import com.sg.widgets.registry.config.BasicPageConfigurator;
 import com.sg.widgets.registry.config.IPageDelegator;
 
-public class ProjectBudgetOfCreateProjectPage implements IPageDelegator, IFormPart {
+public class ProjectBudgetOfCreateProjectPage implements IPageDelegator,
+		IFormPart, IPrimaryObjectValueChangeListener {
 	private TreeViewer viewer;
 	private ProjectBudget root;
 	private boolean isDirty;
 	private IManagedForm form;
-	
-	//TODO 需要修改
-	
+	private TaskForm taskForm;
+
+	// TODO 需要修改
+
 	public ProjectBudgetOfCreateProjectPage() {
 	}
 
 	@Override
 	public Composite createPageContent(Composite parent,
 			PrimaryObjectEditorInput input, BasicPageConfigurator conf) {
-		Project project = (Project) input.getData();
 
-		root = project.getBudget();
+		taskForm = (TaskForm) input.getData();
+
+		taskForm.addFieldValueListener("projecttemplate_id", this);
 
 		viewer = new TreeViewer(parent, SWT.FULL_SELECTION);
 		viewer.getTree().setHeaderVisible(true);
@@ -49,69 +61,96 @@ public class ProjectBudgetOfCreateProjectPage implements IPageDelegator, IFormPa
 		column.getColumn().setText("预算科目");
 		column.getColumn().setWidth(280);
 		column.setLabelProvider(new ColumnLabelProvider());
-		
+
 		column = new TreeViewerColumn(viewer, SWT.RIGHT);
 		column.getColumn().setText("预算金额(元)");
 		column.getColumn().setWidth(120);
-		column.setLabelProvider(new ColumnLabelProvider(){
+		column.setLabelProvider(new ColumnLabelProvider() {
 			@Override
 			public String getText(Object element) {
-				ProjectBudget budget = (ProjectBudget)element;
+				ProjectBudget budget = (ProjectBudget) element;
 				Double value = budget.getBudgetValue();
-				if(value!=null){
+				if (value != null) {
 					DecimalFormat df = new DecimalFormat(Utils.NF_RMB_MONEY);
 					return df.format(value);
-				}else{
+				} else {
 					return "";
 				}
 			}
 		});
-		
-		if(input.isEditable()){
+
+		if (input.isEditable()) {
 			EditingSupport es = new EditingSupport(viewer) {
-				
+
 				@Override
 				protected void setValue(Object element, Object value) {
-					ProjectBudget budget = (ProjectBudget)element;
+					ProjectBudget budget = (ProjectBudget) element;
 					Double val = Utils.getDoubleValue(value);
-					
+
 					budget.inputBudgetValue(val);
 					setDirty(true);
 					viewer.refresh();
 				}
-				
+
 				@Override
 				protected Object getValue(Object element) {
-					ProjectBudget budget = (ProjectBudget)element;
+					ProjectBudget budget = (ProjectBudget) element;
 					Double value = budget.getBudgetValue();
-					if(value == null){
+					if (value == null) {
 						return "";
-					}else{
-						return ""+value;
+					} else {
+						return "" + value;
 					}
 				}
-				
+
 				@Override
 				protected CellEditor getCellEditor(Object element) {
 					return new TextCellEditor(viewer.getTree());
 				}
-				
+
 				@Override
 				protected boolean canEdit(Object element) {
-					ProjectBudget budget = (ProjectBudget)element;
+					ProjectBudget budget = (ProjectBudget) element;
 					return !budget.hasChildren();
 				}
 			};
 			column.setEditingSupport(es);
-			
+
 		}
-		
-		
-		viewer.setInput(root.getChildren());
-		viewer.expandAll();
+
+		setViewerData();
 		return (Composite) viewer.getControl();
 	}
 
+	private void setViewerData() {
+		Object projectTemplateId = taskForm.getValue("projecttemplate_id");
+		if(projectTemplateId == null){
+			return;
+		}
+		
+		ProjectTemplate template = ModelService.createModelObject(
+				ProjectTemplate.class,
+				(ObjectId) projectTemplateId);
+		
+		PrimaryObject budgetItem;
+		List<PrimaryObject> budgetItems = template.getBudgetItems();
+		if(budgetItems != null && budgetItems.size() >0){
+			budgetItem = budgetItems.get(0);
+		} else {
+			return;
+		}
+		
+//		DBObject tgtData = new BasicDBObject();
+//		tgtData.put(ProjectBudget.F_DESC, getDesc());
+//		tgtData.put(ProjectBudget.F_DESC_EN, getDesc_e());
+//		tgtData.put(ProjectBudget.F_CHILDREN,
+//				srcdata.get(BudgetItem.F_CHILDREN));
+
+//		ModelService.createModelObject(tgtData, ProjectBudget.class);
+		
+		viewer.setInput(root.getChildren());
+		viewer.expandAll();
+	}
 
 	@Override
 	public IFormPart getFormPart() {
@@ -132,8 +171,8 @@ public class ProjectBudgetOfCreateProjectPage implements IPageDelegator, IFormPa
 	public boolean isDirty() {
 		return isDirty;
 	}
-	
-	private void setDirty(boolean b){
+
+	private void setDirty(boolean b) {
 		isDirty = b;
 		form.dirtyStateChanged();
 	}
@@ -149,7 +188,6 @@ public class ProjectBudgetOfCreateProjectPage implements IPageDelegator, IFormPa
 			setDirty(false);
 		}
 	}
-
 
 	@Override
 	public boolean setFormInput(Object input) {
@@ -174,5 +212,13 @@ public class ProjectBudgetOfCreateProjectPage implements IPageDelegator, IFormPa
 	public boolean canRefresh() {
 		return false;
 	}
+
+	@Override
+	public void valueChanged(String key, Object oldValue, Object newValue) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
 
 }
