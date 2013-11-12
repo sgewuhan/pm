fad1f052c102eba3139482795d19002f140308f7
yangjun1985 <yj@yaozheng.com.cn>
2013-11-12 20:36:18 +0800

    Signed-off-by: yangjun1985 <yj@yaozheng.com.cn>

--- com.sg.business.model/src/com/sg/business/model/bpmservice/bpmserviceCreateStandloneworService.java
+++ com.sg.business.model/src/com/sg/business/model/bpmservice/bpmserviceCreateStandloneworService.java
@@ -1,6 +1,5 @@
 package com.sg.business.model.bpmservice;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -10,13 +9,9 @@ import org.bson.types.ObjectId;
 import com.mobnut.db.model.IContext;
 import com.mobnut.db.model.ModelService;
 import com.mobnut.db.model.PrimaryObject;
-import com.mongodb.BasicDBObject;
 import com.mongodb.DBObject;
 import com.sg.bpm.service.task.ServiceProvider;
 import com.sg.bpm.workflow.utils.WorkflowUtils;
-import com.sg.business.model.Document;
-import com.sg.business.model.IProcessControl;
-import com.sg.business.model.IWorkCloneFields;
 import com.sg.business.model.Work;
 import com.sg.business.model.WorkDefinition;
 
--- com.sg.business.taskforms/src/com/sg/business/taskforms/actor/SupervisorOfLauncher.java
+++ com.sg.business.taskforms/src/com/sg/business/taskforms/actor/SupervisorOfLauncher.java
@@ -5,7 +5,6 @@ import java.util.List;
 import com.mobnut.db.model.PrimaryObject;
 import com.sg.bpm.service.actor.IActorIdProvider;
 import com.sg.business.model.Organization;
-import com.sg.business.model.Project;
 import com.sg.business.model.Role;
 import com.sg.business.model.RoleAssignment;
 import com.sg.business.model.User;
@@ -20,8 +19,8 @@ public class SupervisorOfLauncher implements IActorIdProvider {
 	@Override
 	public String getActorId(Object[] input) {
 		Work work = (Work) input[0];
-		Project project = work.getProject();
-		Organization org = project.getFunctionOrganization();
+		User charger = work.getCharger();
+		Organization org = charger.getOrganization();
 		Role role = org.getRole(Role.ROLE_DEPT_MANAGER_ID, 0);
 		if (role != null) {
 			List<PrimaryObject> assignment = role.getAssignment();
