69959e31b7cc6a655e3247092a3f79d30e0624ec
yangjun1985 <yj@yaozheng.com.cn>
2013-11-15 16:18:21 +0800

    Signed-off-by: yangjun1985 <yj@yaozheng.com.cn>

---
+++ com.tmt.xt/src/com/tmt/xt/handler/ProjectApplicationWorkOrderOfXT.java
@@ -0,0 +1,36 @@
+package com.tmt.xt.handler;
+
+import org.jbpm.task.Task;
+
+import com.mobnut.db.model.PrimaryObject;
+import com.sg.bpm.workflow.taskform.ITaskFormInputHandler;
+import com.sg.bpm.workflow.taskform.TaskFormConfig;
+import com.sg.business.model.TaskForm;
+import com.sg.widgets.part.CurrentAccountContext;
+
+public class ProjectApplicationWorkOrderOfXT implements ITaskFormInputHandler {
+
+	public ProjectApplicationWorkOrderOfXT() {
+	}
+
+	@Override
+	public PrimaryObject getTaskFormInputData(PrimaryObject taskFormData,
+			TaskFormConfig taskFormConfig, Task task) {
+		if (taskFormData instanceof TaskForm) {
+			TaskForm taskForm = (TaskForm) taskFormData;
+			Object workOrder;
+			try {
+				workOrder = taskForm.getProcessInstanceVarible("prj_number",
+						new CurrentAccountContext());
+				if (workOrder instanceof String) {
+					taskForm.setValue("prj_number", workOrder);
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return taskForm;
+		}
+		return null;
+	}
+
+}
