146ac0ceb513470f34e57208551ce8d9ef222dd1
ghuazh <ghuazh@gmail.com>
2013-11-29 09:22:02 +0800

    Signed-off-by: ghuazh <ghuazh@gmail.com>

---
+++ com.sg.business.visualization/src/com/sg/business/visualization/editor/ProjectSetDashboardRevenue.java
@@ -0,0 +1,31 @@
+package com.sg.business.visualization.editor;
+
+import org.eclipse.swt.widgets.Composite;
+
+import com.sg.widgets.part.NavigatorControl;
+import com.sg.widgets.part.editor.IEditorActionListener;
+import com.sg.widgets.part.editor.PrimaryObjectEditorInput;
+import com.sg.widgets.part.editor.page.INavigatorPageBodyPartCreater;
+import com.sg.widgets.part.editor.page.NavigatorPage;
+
+
+public class ProjectSetDashboardRevenue implements INavigatorPageBodyPartCreater {
+
+  public ProjectSetDashboardRevenue() {
+    // TODO Auto-generated constructor stub
+  }
+
+  @Override
+  public void editorAction( IEditorActionListener reciever, int code, Object object ) {
+    // TODO Auto-generated method stub
+  }
+
+  @Override
+  public void createNavigatorBody( Composite body,
+                                   NavigatorControl navi,
+                                   PrimaryObjectEditorInput input,
+                                   NavigatorPage navigatorPage )
+  {
+    // TODO Auto-generated method stub
+  }
+}
