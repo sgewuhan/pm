d62904760b75e0afff36b3edaba11481e995b8da
ghuazh <ghuazh@gmail.com>
2013-11-30 22:30:33 +0800

    改善图表显示的性能 Signed-off-by: ghuazh <ghuazh@gmail.com>

--- com.sg.business.visualization/src/com/sg/business/visualization/editor/ProjectSetDashboardSchedual.java
+++ com.sg.business.visualization/src/com/sg/business/visualization/editor/ProjectSetDashboardSchedual.java
@@ -1,5 +1,6 @@
 package com.sg.business.visualization.editor;
 
+import org.eclipse.birt.chart.model.Chart;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.SashForm;
 import org.eclipse.swt.layout.FillLayout;
@@ -11,6 +12,7 @@ import org.eclipse.swt.widgets.TabItem;
 
 import com.sg.business.model.ProjectProvider;
 import com.sg.business.visualization.chart.ProjectChartFoctory;
+import com.sg.widgets.MessageUtil;
 import com.sg.widgets.birtcharts.ChartCanvas;
 import com.sg.widgets.viewer.ViewerControl;
 
@@ -28,6 +30,7 @@ public class ProjectSetDashboardSchedual extends AbstractProjectPage {
 
 	@Override
 	protected Composite createContent(Composite body) {
+		//
 		SashForm content = new SashForm(body, SWT.HORIZONTAL);
 		Composite tableContent = new Composite(content, SWT.NONE);
 		navi.createPartContent(tableContent);
@@ -37,6 +40,7 @@ public class ProjectSetDashboardSchedual extends AbstractProjectPage {
 		createGraphic(graphicContent);
 
 		content.setWeights(new int[] { 3, 2 });
+
 		return content;
 	}
 
@@ -55,26 +59,47 @@ public class ProjectSetDashboardSchedual extends AbstractProjectPage {
 		tabFolder = new TabFolder(parent, SWT.TOP);
 		TabItem pieTabItem = new TabItem(tabFolder, SWT.NONE);
 		pieTabItem.setText("进度摘要");
-		statusPieChart = new ChartCanvas(tabFolder, SWT.NONE);
+		statusPieChart = new ChartCanvas(tabFolder, SWT.NONE) {
+			@Override
+			public Chart getChart() {
+				return getStatusPieChartData();
+			}
+		};
 		pieTabItem.setControl(statusPieChart);
 
 		TabItem meterTabItem = new TabItem(tabFolder, SWT.NONE);
 		meterTabItem.setText("仪表盘");
 		Composite composite = new Composite(tabFolder, SWT.NONE);
 		composite.setLayout(new GridLayout());
-		finishedProjectMeter = new ChartCanvas(composite, SWT.NONE);
+		finishedProjectMeter = new ChartCanvas(composite, SWT.NONE) {
+			@Override
+			public Chart getChart() {
+				return getFinishedProjectMeterChartData();
+			}
+		};
 		finishedProjectMeter.setLayoutData(new GridData(SWT.FILL, SWT.FILL,
 				true, true, 1, 1));
 
-		processProjectMeter = new ChartCanvas(composite, SWT.NONE);
+		processProjectMeter = new ChartCanvas(composite, SWT.NONE) {
+			@Override
+			public Chart getChart() {
+				return getProcessProjectMeterChartData();
+			}
+		};
 		processProjectMeter.setLayoutData(new GridData(SWT.FILL, SWT.FILL,
 				true, true, 1, 1));
 
-		allProjectMeter = new ChartCanvas(composite, SWT.NONE);
+		allProjectMeter = new ChartCanvas(composite, SWT.NONE) {
+
+			@Override
+			public Chart getChart() {
+				return getAllProjectMeterChartData();
+			}
+
+		};
 		allProjectMeter.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true,
 				true, 1, 1));
 		meterTabItem.setControl(composite);
-
 		loadChartData();
 	}
 
@@ -102,110 +127,224 @@ public class ProjectSetDashboardSchedual extends AbstractProjectPage {
 	}
 
 	private void loadChartData() {
+		// long start = System.currentTimeMillis();
+		//
+		// // "正常完成"
+		// int value1 = data.summaryData.finished_normal;
+		// // "超期完成",
+		// int value2 = data.summaryData.finished_delay;
+		// // "提前完成",
+		// int value3 = data.summaryData.finished_advance;
+		// // "进度延迟",
+		// int value4 = data.summaryData.processing_delay;
+		// // "正常进行"
+		// int value5 = data.summaryData.processing_normal;
+		// // 进度提前
+		// int value6 = data.summaryData.processing_advance;
+		// int sum = value1 + value2 + value3 + value4 + value5 + value6;
+		// double allProjectOverTimeRate = sum == 0 ? 0
+		// : (100d * (value2 + value4) / sum);
+		//
+		// sum = value4 + value5 + value6;
+		// double processProjectOverTimeRate = sum == 0 ? 0
+		// : (100d * value4 / sum);
+		//
+		// sum = value1 + value2 + value3;
+		// double finishProjectOverTimeRate = sum == 0 ? 0 : (100d * value2 /
+		// sum);
+		//
+		// finishedProjectMeter.setChart(ProjectChartFoctory.createMeterChart(
+		// "已完成项目超期 ", "进度延迟", finishProjectOverTimeRate));
+		// processProjectMeter.setChart(ProjectChartFoctory.createMeterChart(
+		// "进行中项目超期 ", "进度延迟", processProjectOverTimeRate));
+		// allProjectMeter.setChart(ProjectChartFoctory.createMeterChart(
+		// "整体项目超期 ", "进度延迟", allProjectOverTimeRate));
+		//
+		// long end = System.currentTimeMillis();
+		// System.out.print("Load Dash:");
+		// System.out.println(end - start);
+		// start = System.currentTimeMillis();
+		//
+		// //
+		// *****************************************************************************************
+		// String pieChartCaption = "进度摘要";
+		// String[] texts = new String[] { "正常完成", "超期完成", "进度延迟", "正常进行",
+		// "进度提前" };
+		// double[] values = new double[] { (value1 + value3), value2, value4,
+		// value5, value6 };
+		// statusPieChart.setChart(ProjectChartFoctory.createPieChart(
+		// pieChartCaption, texts, values));
+		//
+		// end = System.currentTimeMillis();
+		// System.out.print("Load Pie:");
+		// System.out.println(end - start);
+		// start = System.currentTimeMillis();
+
+		// *****************************************************************************************
+		String[] deptParameter = new String[data.sum.subOrganizationProjectProvider
+				.size()];
+		if (deptParameter.length != 0) {
+			if (deptBarTabItem == null) {
+				deptBarTabItem = new TabItem(tabFolder, SWT.NONE);
+				deptBarTabItem.setText("项目承担部门");
+				deptProjectBar = new ChartCanvas(tabFolder, SWT.NONE) {
+					@Override
+					public Chart getChart() {
+						return getDeptBarChartData();
+					}
+				};
+				deptBarTabItem.setControl(deptProjectBar);
+			}
+		}
+		// *****************************************************************************************
+		String[] chargerName = new String[data.sum.subChargerProjectProvider
+				.size()];
+		if (chargerName.length != 0) {
+			if (pmBarTabItem == null) {
+				pmBarTabItem = new TabItem(tabFolder, SWT.NONE);
+				pmBarTabItem.setText("项目经理");
+				pmProjectBar = new ChartCanvas(tabFolder, SWT.NONE) {
+					@Override
+					public Chart getChart() {
+						return getProjectChargeBarChartData();
+					}
+				};
+
+				pmBarTabItem.setControl(pmProjectBar);
+			}
+		}
+
+	}
+
+	protected Chart getProjectChargeBarChartData() {
+		String[] chargerName = new String[data.sum.subChargerProjectProvider
+				.size()];
+		double[] userValue1 = new double[data.sum.subChargerProjectProvider
+				.size()];
+		double[] userValue2 = new double[data.sum.subChargerProjectProvider
+				.size()];
+		for (int i = 0; i < chargerName.length; i++) {
+			ProjectProvider projectProvider = data.sum.subChargerProjectProvider
+					.get(i);
+			projectProvider.setParameters(data.parameters);
+			projectProvider.getData();
+			chargerName[i] = projectProvider.getDesc();
+			userValue1[i] = projectProvider.sum.processing_normal
+					+ projectProvider.sum.processing_advance;
+			userValue2[i] = projectProvider.sum.processing_delay;
+		}
+
+		return ProjectChartFoctory.createStackedBarChart("项目经理项目执行状况",
+				chargerName, userValue1, userValue2,
+				new String[] { "正常", "超期" });
+	}
+
+	private Chart getDeptBarChartData() {
+		String[] deptParameter = new String[data.sum.subOrganizationProjectProvider
+				.size()];
+		double[] deptValue1 = new double[data.sum.subOrganizationProjectProvider
+				.size()];
+		double[] deptValue2 = new double[data.sum.subOrganizationProjectProvider
+				.size()];
+		for (int i = 0; i < deptParameter.length; i++) {
+			ProjectProvider projectProvider = data.sum.subOrganizationProjectProvider
+					.get(i);
+			projectProvider.setParameters(data.parameters);
+			projectProvider.getData();
+			deptParameter[i] = projectProvider.getDesc();
+			deptValue1[i] = projectProvider.sum.processing_normal
+					+ projectProvider.sum.processing_advance;
+			deptValue2[i] = projectProvider.sum.processing_delay;
+		}
+
+		return ProjectChartFoctory.createStackedBarChart("部门项目执行状况",
+				deptParameter, deptValue1, deptValue2, new String[] { "正常",
+						"超期" });
+	}
+
+	private void redrawChart() {
+		try {
+			allProjectMeter.redrawChart();
+			deptProjectBar.redrawChart();
+			finishedProjectMeter.redrawChart();
+			processProjectMeter.redrawChart();
+			statusPieChart.redrawChart();
+		} catch (Exception e) {
+			MessageUtil.showToast(e);
+		}
+	}
+
+	private Chart getAllProjectMeterChartData() {
 		// "正常完成"
-		int value1 = data.summaryData.finished_normal;
+		int value1 = data.sum.finished_normal;
 		// "超期完成",
-		int value2 = data.summaryData.finished_delay;
+		int value2 = data.sum.finished_delay;
 		// "提前完成",
-		int value3 = data.summaryData.finished_advance;
+		int value3 = data.sum.finished_advance;
 		// "进度延迟",
-		int value4 = data.summaryData.processing_delay;
+		int value4 = data.sum.processing_delay;
 		// "正常进行"
-		int value5 = data.summaryData.processing_normal;
+		int value5 = data.sum.processing_normal;
 		// 进度提前
-		int value6 = data.summaryData.processing_advance;
+		int value6 = data.sum.processing_advance;
 		int sum = value1 + value2 + value3 + value4 + value5 + value6;
 		double allProjectOverTimeRate = sum == 0 ? 0
 				: (100d * (value2 + value4) / sum);
+		return ProjectChartFoctory.createMeterChart("整体项目超期 ", "进度延迟",
+				allProjectOverTimeRate);
+	}
 
-		sum = value4 + value5 + value6;
+	private Chart getProcessProjectMeterChartData() {
+		// "进度延迟",
+		int value4 = data.sum.processing_delay;
+		// "正常进行"
+		int value5 = data.sum.processing_normal;
+		// 进度提前
+		int value6 = data.sum.processing_advance;
+
+		int sum = value4 + value5 + value6;
 		double processProjectOverTimeRate = sum == 0 ? 0
 				: (100d * value4 / sum);
 
-		sum = value1 + value2 + value3;
+		return ProjectChartFoctory.createMeterChart("进行中项目超期 ", "进度延迟",
+				processProjectOverTimeRate);
+
+	}
+
+	private Chart getFinishedProjectMeterChartData() {
+		// "正常完成"
+		int value1 = data.sum.finished_normal;
+		// "超期完成",
+		int value2 = data.sum.finished_delay;
+		// "提前完成",
+		int value3 = data.sum.finished_advance;
+		int sum = value1 + value2 + value3;
 		double finishProjectOverTimeRate = sum == 0 ? 0 : (100d * value2 / sum);
 
-		finishedProjectMeter.setChart(ProjectChartFoctory.createMeterChart(
-				"已完成项目超期 ", "进度延迟", finishProjectOverTimeRate));
-		processProjectMeter.setChart(ProjectChartFoctory.createMeterChart(
-				"进行中项目超期 ", "进度延迟", processProjectOverTimeRate));
-		allProjectMeter.setChart(ProjectChartFoctory.createMeterChart(
-				"整体项目超期 ", "进度延迟", allProjectOverTimeRate));
+		return ProjectChartFoctory.createMeterChart("已完成项目超期 ", "进度延迟",
+				finishProjectOverTimeRate);
+	}
 
-		// *****************************************************************************************
+	private Chart getStatusPieChartData() {
+		// "正常完成"
+		int value1 = data.sum.finished_normal;
+		// "超期完成",
+		int value2 = data.sum.finished_delay;
+		// "提前完成",
+		int value3 = data.sum.finished_advance;
+		// "进度延迟",
+		int value4 = data.sum.processing_delay;
+		// "正常进行"
+		int value5 = data.sum.processing_normal;
+		// 进度提前
+		int value6 = data.sum.processing_advance;
+
 		String pieChartCaption = "进度摘要";
 		String[] texts = new String[] { "正常完成", "超期完成", "进度延迟", "正常进行", "进度提前" };
 		double[] values = new double[] { (value1 + value3), value2, value4,
 				value5, value6 };
-		statusPieChart.setChart(ProjectChartFoctory.createPieChart(
-				pieChartCaption, texts, values));
-
-		// *****************************************************************************************
-		String[] deptParameter = new String[data.summaryData.subOrganizationProjectProvider
-				.size()];
-		if (deptParameter.length != 0) {
-			double[] deptValue1 = new double[data.summaryData.subOrganizationProjectProvider
-					.size()];
-			double[] deptValue2 = new double[data.summaryData.subOrganizationProjectProvider
-					.size()];
-			for (int i = 0; i < deptParameter.length; i++) {
-				ProjectProvider projectProvider = data.summaryData.subOrganizationProjectProvider
-						.get(i);
-				projectProvider.setParameters(data.parameters);
-				projectProvider.getData();
-				deptParameter[i] = projectProvider.getDesc();
-				deptValue1[i] = projectProvider.summaryData.processing_normal
-						+ projectProvider.summaryData.processing_advance;
-				deptValue2[i] = projectProvider.summaryData.processing_delay;
-			}
-
-			if (deptBarTabItem == null) {
-				deptBarTabItem = new TabItem(tabFolder, SWT.NONE);
-				deptBarTabItem.setText("项目承担部门");
-				deptProjectBar = new ChartCanvas(tabFolder, SWT.NONE);
-				deptBarTabItem.setControl(deptProjectBar);
-			}
-
-			deptProjectBar.setChart(ProjectChartFoctory.createStackedBarChart(
-					"部门项目执行状况", deptParameter, deptValue1, deptValue2,new String[]{"正常","超期"}));
-		}
-		// *****************************************************************************************
-		String[] chargerName = new String[data.summaryData.subChargerProjectProvider
-				.size()];
-		if (chargerName.length != 0) {
-			double[] userValue1 = new double[data.summaryData.subChargerProjectProvider
-					.size()];
-			double[] userValue2 = new double[data.summaryData.subChargerProjectProvider
-					.size()];
-			for (int i = 0; i < chargerName.length; i++) {
-				ProjectProvider projectProvider = data.summaryData.subChargerProjectProvider
-						.get(i);
-				projectProvider.setParameters(data.parameters);
-				projectProvider.getData();
-				chargerName[i] = projectProvider.getDesc();
-				userValue1[i] = projectProvider.summaryData.processing_normal
-						+ projectProvider.summaryData.processing_advance;
-				userValue2[i] = projectProvider.summaryData.processing_delay;
-			}
-
-			if(pmBarTabItem==null){
-				pmBarTabItem = new TabItem(tabFolder, SWT.NONE);
-				pmBarTabItem.setText("项目经理");
-				pmProjectBar = new ChartCanvas(tabFolder, SWT.NONE);
-				pmBarTabItem.setControl(pmProjectBar);
-			}
-
-			pmProjectBar.setChart(ProjectChartFoctory.createStackedBarChart(
-					"项目经理项目执行状况", chargerName, userValue1, userValue2,new String[]{"正常","超期"}));
-		}
-	}
-
-	private void redrawChart() {
-		allProjectMeter.redrawChart();
-		deptProjectBar.redrawChart();
-		finishedProjectMeter.redrawChart();
-		processProjectMeter.redrawChart();
-		statusPieChart.redrawChart();
+		return ProjectChartFoctory.createPieChart(pieChartCaption, texts,
+				values);
 	}
 
 }
