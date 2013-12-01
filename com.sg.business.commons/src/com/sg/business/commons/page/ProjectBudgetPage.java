ca9f3da6c10c0b8fc275b7163668b5e9545168d5
jinxitao <jxt@yaozheng.com.cn>
2013-12-01 16:29:01 +0800

    Signed-off-by: jinxitao <jxt@yaozheng.com.cn>

--- com.sg.business.commons/src/com/sg/business/commons/page/ProjectBudgetPage.java
+++ com.sg.business.commons/src/com/sg/business/commons/page/ProjectBudgetPage.java
@@ -1,6 +1,9 @@
 package com.sg.business.commons.page;
 
 import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 import org.eclipse.jface.viewers.CellEditor;
 import org.eclipse.jface.viewers.ColumnLabelProvider;
@@ -8,14 +11,26 @@ import org.eclipse.jface.viewers.EditingSupport;
 import org.eclipse.jface.viewers.TextCellEditor;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.jface.viewers.TreeViewerColumn;
+import org.eclipse.rap.rwt.RWT;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.layout.FormAttachment;
+import org.eclipse.swt.layout.FormData;
+import org.eclipse.swt.layout.FormLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.forms.IFormPart;
 import org.eclipse.ui.forms.IManagedForm;
 
 import com.mobnut.commons.util.Utils;
+import com.mobnut.commons.util.file.ExcelExportJob;
+import com.mobnut.commons.util.file.IColumnExportDefinition;
+import com.mobnut.commons.util.file.IExportValueDelegator;
+import com.mobnut.db.model.PrimaryObject;
 import com.sg.business.model.Project;
 import com.sg.business.model.ProjectBudget;
+import com.sg.business.resource.BusinessResource;
 import com.sg.widgets.part.CurrentAccountContext;
 import com.sg.widgets.part.editor.PrimaryObjectEditorInput;
 import com.sg.widgets.registry.config.BasicPageConfigurator;
@@ -34,8 +49,15 @@ public class ProjectBudgetPage implements IPageDelegator, IFormPart {
 	@Override
 	public Composite createPageContent(Composite parent,
 			PrimaryObjectEditorInput input, BasicPageConfigurator conf) {
-		Project project = (Project) input.getData();
 
+		parent.setLayout(new FormLayout());
+		Button b = createButton(parent);
+		FormData fd = new FormData();
+		b.setLayoutData(fd);
+		fd.top = new FormAttachment(0, 4);
+		fd.right = new FormAttachment(100, -4);
+
+		Project project = (Project) input.getData();
 		root = project.getBudget();
 
 		viewer = new TreeViewer(parent, SWT.FULL_SELECTION);
@@ -47,72 +69,202 @@ public class ProjectBudgetPage implements IPageDelegator, IFormPart {
 		column.getColumn().setText("预算科目");
 		column.getColumn().setWidth(280);
 		column.setLabelProvider(new ColumnLabelProvider());
-		
+
 		column = new TreeViewerColumn(viewer, SWT.RIGHT);
 		column.getColumn().setText("预算金额(元)");
 		column.getColumn().setWidth(120);
-		column.setLabelProvider(new ColumnLabelProvider(){
+		column.setLabelProvider(new ColumnLabelProvider() {
 			@Override
 			public String getText(Object element) {
-				ProjectBudget budget = (ProjectBudget)element;
+				ProjectBudget budget = (ProjectBudget) element;
 				Double value = budget.getBudgetValue();
-				if(value!=null){
+				if (value != null) {
 					DecimalFormat df = new DecimalFormat(Utils.NF_RMB_MONEY);
 					return df.format(value);
-				}else{
+				} else {
 					return "";
 				}
 			}
 		});
-		
-		if(input.isEditable()){
+
+		if (input.isEditable()) {
 			EditingSupport es = new EditingSupport(viewer) {
-				
+
 				@Override
 				protected void setValue(Object element, Object value) {
-					ProjectBudget budget = (ProjectBudget)element;
+					ProjectBudget budget = (ProjectBudget) element;
 					Double val = Utils.getDoubleValue(value);
-					
+
 					budget.inputBudgetValue(val);
 					setDirty(true);
 					viewer.refresh();
 				}
-				
+
 				@Override
 				protected Object getValue(Object element) {
-					ProjectBudget budget = (ProjectBudget)element;
+					ProjectBudget budget = (ProjectBudget) element;
 					Double value = budget.getBudgetValue();
-					if(value == null){
+					if (value == null) {
 						return "";
-					}else{
-						return ""+value;
+					} else {
+						return "" + value;
 					}
 				}
-				
+
 				@Override
 				protected CellEditor getCellEditor(Object element) {
 					return new TextCellEditor(viewer.getTree());
 				}
-				
+
 				@Override
 				protected boolean canEdit(Object element) {
-					ProjectBudget budget = (ProjectBudget)element;
+					ProjectBudget budget = (ProjectBudget) element;
 					return !budget.hasChildren();
 				}
 			};
 			column.setEditingSupport(es);
-			
+
 		}
-		
-		if(root == null){
+
+		if (root == null) {
 			viewer.setInput(new ProjectBudget[0]);
-		}else{
+		} else {
 			viewer.setInput(root.getChildren());
 		}
+		fd = new FormData();
+		viewer.getControl().setLayoutData(fd);
+		fd.top = new FormAttachment(b, 4);
+		fd.left = new FormAttachment(0, 4);
+		fd.right = new FormAttachment(100, -4);
+		fd.bottom = new FormAttachment(100, -4);
+
 		viewer.expandAll();
 		return (Composite) viewer.getControl();
 	}
 
+	private Button createButton(final Composite parent) {
+		Button exportButton = new Button(parent, SWT.PUSH);
+		exportButton.setImage(BusinessResource
+				.getImage(BusinessResource.IMAGE_EXPORT_24));
+		exportButton.setToolTipText("导出");
+		exportButton.setData(RWT.CUSTOM_VARIANT, "whitebutton");
+		exportButton.addSelectionListener(new SelectionListener() {
+
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				ExcelExportJob job = new ExcelExportJob("预算");
+
+				IColumnExportDefinition[] columns = new IColumnExportDefinition[2];
+				columns[0] = new IColumnExportDefinition() {
+
+					@Override
+					public String getName() {
+						return "预算科目";
+					}
+
+					@Override
+					public String getType() {
+						return Utils.TYPE_STRING;
+					}
+
+					@Override
+					public int getWidth() {
+						return 0;
+					}
+
+					@Override
+					public String getColumn() {
+						return "desc";
+					}
+
+					@Override
+					public IExportValueDelegator getExportValueDelegator() {
+						return new IExportValueDelegator() {
+							@Override
+							public Object getValue(
+									Map<String, Object> dataRow,
+									IColumnExportDefinition iColumnExportDefinition) {
+
+								String dataRowValue = (String) dataRow
+										.get("desc");
+								Object level = dataRow.get("level");
+								if (level instanceof Integer) {
+									for (int i = 0; i < (Integer) level; i++) {
+										dataRowValue = "   " + dataRowValue;
+									}
+								}
+
+								return dataRowValue;
+							}
+
+						};
+					}
+
+				};
+
+				columns[1] = new IColumnExportDefinition() {
+
+					@Override
+					public String getName() {
+						return "预算金额（元）";
+					}
+
+					@Override
+					public String getType() {
+						return Utils.TYPE_DOUBLE;
+					}
+
+					@Override
+					public int getWidth() {
+						return 0;
+					}
+
+					@Override
+					public String getColumn() {
+						return "budgetvalue";
+					}
+
+					@Override
+					public IExportValueDelegator getExportValueDelegator() {
+						return null;
+					}
+
+				};
+
+				Object input = viewer.getInput();
+				job.setColumnExportDefinition(columns);
+				job.setInput(getExportData(((Object[]) input)[0], 0));
+				job.setUser(true);
+				job.setFormat(false);
+				job.start(viewer.getControl().getDisplay());
+			}
+
+			private List<PrimaryObject> getExportData(Object obj, int level) {
+				List<PrimaryObject> result = new ArrayList<PrimaryObject>();
+				if (obj instanceof ProjectBudget) {
+					ProjectBudget projectBudget = (ProjectBudget) obj;
+					projectBudget.setValue("level", level);
+					result.add(projectBudget);
+					ProjectBudget[] children = projectBudget.getChildren();
+					if (children != null) {
+						for (int i = 0; i < children.length; i++) {
+							ProjectBudget child = children[i];
+							child.setValue("level", level);
+							result.addAll(getExportData(child, level + 1));
+						}
+					}
+				}
+				return result;
+			}
+
+			@Override
+			public void widgetDefaultSelected(SelectionEvent e) {
+
+			}
+		});
+
+		return exportButton;
+	}
 
 	@Override
 	public IFormPart getFormPart() {
@@ -133,8 +285,8 @@ public class ProjectBudgetPage implements IPageDelegator, IFormPart {
 	public boolean isDirty() {
 		return isDirty;
 	}
-	
-	private void setDirty(boolean b){
+
+	private void setDirty(boolean b) {
 		isDirty = b;
 		form.dirtyStateChanged();
 	}
@@ -150,7 +302,6 @@ public class ProjectBudgetPage implements IPageDelegator, IFormPart {
 			setDirty(false);
 		}
 	}
-
 
 	@Override
 	public boolean setFormInput(Object input) {
