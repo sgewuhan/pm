3e0fb145b9dd8070e8f1784b213678d378453ed7
jinxitao <jxt@yaozheng.com.cn>
2013-12-05 09:06:39 +0800

    Signed-off-by: jinxitao <jxt@yaozheng.com.cn>

--- com.sg.business.project/src/com/sg/business/project/labelprovider/MinusTime.java
+++ com.sg.business.project/src/com/sg/business/project/labelprovider/MinusTime.java
@@ -4,12 +4,6 @@ import java.util.Date;
 
 import org.eclipse.jface.viewers.ColumnLabelProvider;
 
-import com.mobnut.db.DBActivator;
-import com.mobnut.db.model.ModelService;
-import com.mongodb.BasicDBObject;
-import com.mongodb.DBCollection;
-import com.mongodb.DBObject;
-import com.sg.business.model.IModelConstants;
 import com.sg.business.model.UserTask;
 
 public class MinusTime extends ColumnLabelProvider {
@@ -18,23 +12,41 @@ public class MinusTime extends ColumnLabelProvider {
 	public String getText(Object element) {
 		if (element instanceof UserTask) {
 			UserTask userTask = (UserTask) element;
-			Object value = userTask.getValue(UserTask.F_WORKITEMID);
-			DBCollection collection = DBActivator.getCollection(
-					IModelConstants.DB, IModelConstants.C_USERTASK);
+			// Object value = userTask.getValue(UserTask.F_WORKITEMID);
+			// DBCollection collection = DBActivator.getCollection(
+			// IModelConstants.DB, IModelConstants.C_USERTASK);
+			//
+			// DBObject inProgress = collection.findOne(new BasicDBObject()
+			// .append(UserTask.F_WORKITEMID, value).append(
+			// UserTask.F_STATUS, "InProgress"));
+			//
+			// if (inProgress != null) {
+			// UserTask taskInProgress = ModelService.createModelObject(
+			// inProgress, UserTask.class);
+			// long minus = taskInProgress.get_cdate().getTime()
+			// - userTask.get_cdate().getTime();
+			// long hh = minus / (1000 * 60 * 60);
+			// long mm = (minus - hh * 1000 * 60 * 60) / (1000 * 60);
+			// long ss = (minus - hh * 1000 * 60 * 60 - mm * 1000 * 60) / 1000;
+			// return hh + "小时" + mm + "分钟" + ss + "秒";
+			// } else {
+			// long minus = new Date().getTime()
+			// - userTask.get_cdate().getTime();
+			// long hh = minus / (1000 * 60 * 60);
+			// long mm = (minus - hh * 1000 * 60 * 60) / (1000 * 60);
+			// long ss = (minus - hh * 1000 * 60 * 60 - mm * 1000 * 60) / 1000;
+			// return hh + "小时" + mm + "分钟" + ss + "秒";
+			// }
 
-			DBObject inProgress = collection.findOne(new BasicDBObject()
-					.append(UserTask.F_WORKITEMID, value).append(
-							UserTask.F_STATUS, "InProgress"));
-
-			if (inProgress != null) {
-				UserTask taskInProgress = ModelService.createModelObject(
-						inProgress, UserTask.class);
-				long minus = taskInProgress.get_cdate().getTime()
-						- userTask.get_cdate().getTime();
+			Object inProgress = userTask.getValue("InProgress");
+			if (inProgress instanceof Date) {
+				Date date = (Date) inProgress;
+				long minus = date.getTime() - userTask.get_cdate().getTime();
 				long hh = minus / (1000 * 60 * 60);
 				long mm = (minus - hh * 1000 * 60 * 60) / (1000 * 60);
 				long ss = (minus - hh * 1000 * 60 * 60 - mm * 1000 * 60) / 1000;
 				return hh + "小时" + mm + "分钟" + ss + "秒";
+
 			} else {
 				long minus = new Date().getTime()
 						- userTask.get_cdate().getTime();
