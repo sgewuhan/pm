ba8dc874cb4d92be1c3ad2f35ae8d32cc210e199
yangjun1985 <yj@yaozheng.com.cn>
2013-12-16 09:21:52 +0800

    Signed-off-by: yangjun1985 <yj@yaozheng.com.cn>

--- com.sg.business.project/src/com/sg/business/project/setup/ProjectDelete.java
+++ com.sg.business.project/src/com/sg/business/project/setup/ProjectDelete.java
@@ -22,7 +22,7 @@ public class ProjectDelete implements ISchedualJobRunnable {
 
 	@Override
 	public boolean run() {
-		DBCollection col = getCol();
+//		DBCollection col = getCol();
 		// ObjectId _id = new ObjectId("525b4d18f0209adcc595131f");
 		//
 		// Organization org = ModelService.createModelObject(Organization.class,
@@ -30,25 +30,25 @@ public class ProjectDelete implements ISchedualJobRunnable {
 		// List<PrimaryObject> projectList = org.getRelationById(
 		// Organization.F__ID, Project.F_FUNCTION_ORGANIZATION,
 		// Project.class);
-		DBCollection projectCol = getCol();
-		BasicDBObject ref = new BasicDBObject();
-		Calendar c = Calendar.getInstance();
-		c.set(2013, 11, 01);
-		ref.put(Project.F__CDATE,
-				new BasicDBObject().append("$gt", c.getTime()));
-		DBCursor cursor = projectCol.find(ref);
-		while (cursor.hasNext()) {
-			DBObject next = cursor.next();
-			Project project = ModelService.createModelObject(next,
-					Project.class);
-			System.out.println(project.get_id());
+//		DBCollection projectCol = getCol();
+//		BasicDBObject ref = new BasicDBObject();
+//		Calendar c = Calendar.getInstance();
+//		c.set(2013, 11, 01);
+//		ref.put(Project.F__CDATE,
+//				new BasicDBObject().append("$gt", c.getTime()));
+//		DBCursor cursor = projectCol.find(ref);
+//		while (cursor.hasNext()) {
+//			DBObject next = cursor.next();
+//			Project project = ModelService.createModelObject(next,
+//					Project.class);
+//			System.out.println(project.get_id());
 			// try {
 			// project.doRemove(new CurrentAccountContext());
 			// } catch (Exception e) {
 			// }
 			// col.remove(new BasicDBObject().append(Project.F__ID,
 			// project.get_id()));
-		}
+//		}
 
 		// List<PrimaryObject> projectList = null;
 		// for (PrimaryObject po : projectList) {
@@ -189,10 +189,10 @@ public class ProjectDelete implements ISchedualJobRunnable {
 	// }
 	// }
 
-	private DBCollection getCol() {
-		return DBActivator.getCollection(IModelConstants.DB,
-				IModelConstants.C_PROJECT);
-	}
+//	private DBCollection getCol() {
+//		return DBActivator.getCollection(IModelConstants.DB,
+//				IModelConstants.C_PROJECT);
+//	}
 
 	// private DBCollection getCol(String collectionName) {
 	// return DBActivator.getCollection(IModelConstants.DB, collectionName);
