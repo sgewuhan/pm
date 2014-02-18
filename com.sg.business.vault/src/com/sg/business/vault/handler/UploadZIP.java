a0084e46cd7c4df03adbf9ccd01bad13eff79bf8
yangjun1985 <yj@yaozheng.com.cn>
2014-02-18 09:11:20 +0800

    Signed-off-by: yangjun1985 <yj@yaozheng.com.cn>

--- com.sg.business.vault/src/com/sg/business/vault/handler/UploadZIP.java
+++ com.sg.business.vault/src/com/sg/business/vault/handler/UploadZIP.java
@@ -1,8 +1,17 @@
 package com.sg.business.vault.handler;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -17,14 +26,18 @@ import org.eclipse.core.commands.Command;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.IWorkbenchPart;
 
+import com.mobnut.db.file.RemoteFile;
+import com.mobnut.db.file.RemoteFileSet;
 import com.mobnut.db.model.IContext;
 import com.mobnut.db.model.ModelService;
 import com.mobnut.db.model.PrimaryObject;
 import com.mongodb.BasicDBObject;
 import com.sg.business.model.Container;
+import com.sg.business.model.Document;
 import com.sg.business.model.Folder;
 import com.sg.business.model.IModelConstants;
 import com.sg.business.model.Organization;
+import com.sg.business.model.OrganizationDistributeFileBase;
 import com.sg.widgets.command.AbstractNavigatorHandler;
 import com.sg.widgets.part.CurrentAccountContext;
 import com.sg.widgets.viewer.ViewerControl;
@@ -35,7 +48,7 @@ public class UploadZIP extends AbstractNavigatorHandler {
 	protected void execute(PrimaryObject selected, IWorkbenchPart part,
 			ViewerControl vc, Command command, Map<String, Object> parameters,
 			IStructuredSelection selection) {
-		String path = "D:/dssroot/pm2.zip";
+		String path = "D:/dssroot/pm21.zip";
 		try {
 			IContext context = new CurrentAccountContext();
 			Organization org = null;
@@ -59,7 +72,7 @@ public class UploadZIP extends AbstractNavigatorHandler {
 		}
 	}
 
-	@SuppressWarnings({ "unused", "resource" })
+	@SuppressWarnings({ "resource" })
 	private void readZIP(Organization org, String zipPath, IContext context)
 			throws Exception {
 		ZipFile zf = new ZipFile(zipPath);
@@ -79,7 +92,7 @@ public class UploadZIP extends AbstractNavigatorHandler {
 				long size = ze.getSize();
 				if (size > 0) {
 					String name = ze.getName();
-					initDocument(org, context, folderMap, name);
+					initDocument(org, context, folderMap, name, zipPath, zf, ze);
 				}
 			}
 			ze = zin.getNextEntry();
@@ -105,7 +118,9 @@ public class UploadZIP extends AbstractNavigatorHandler {
 	}
 
 	private void initDocument(Organization org, IContext context,
-			Map<String, ObjectId> folderMap, String name) throws Exception {
+			Map<String, ObjectId> folderMap, String name, String zipPath,
+			ZipFile zf, ZipEntry ze) throws Exception {
+		zipPath = zipPath + "/" + name;
 		String[] paths = name.split("/");
 		List<String> list = new ArrayList<String>();
 		for (int i = 0; i < paths.length - 1; i++) {
@@ -113,12 +128,72 @@ public class UploadZIP extends AbstractNavigatorHandler {
 		}
 		ObjectId folder_id = initFolder(org, context, folderMap,
 				list.toArray(new String[0]));
-		createDocument(org, context, folder_id, paths[paths.length - 1]);
+		createDocument(org, context, folder_id, paths[paths.length - 1],
+				zipPath, zf, ze);
 	}
 
 	private void createDocument(Organization org, IContext context,
-			ObjectId folder_id, String name) {
+			ObjectId folder_id, String name, String zipPath, ZipFile zf,
+			ZipEntry ze) throws Exception {
 
+		// Folder folder = ModelService.createModelObject(Folder.class,
+		// folder_id);
+		// Document doc = folder.makeCreateDocument(context);
+		// doc.setValue(Document.F_DESC, name);
+		// doc.doInsert(context);
+
+		// List<RemoteFile> originalRFiles = doc
+		// .getGridFSFileValue(Document.F_VAULT);
+		// OrganizationDistributeFileBase filebase = new
+		// OrganizationDistributeFileBase();
+		// String fileDB = filebase.getDB(doc);
+		// String namespace = filebase.getNamespace(doc);
+		// RemoteFileSet fileSet = new RemoteFileSet(fileDB, namespace);
+		// fileSet.setOriginalRemoteFileSet(originalRFiles);
+		// fileSet.setUpdatedRemoteFileSet(originalRFiles);
+
+		String b;
+		File file = new File("D:/dssroot/" + name);
+		// OutputStream outputStream = new BufferedOutputStream(
+		// new FileOutputStream(file));
+		// InputStream inputStream = new
+		// BufferedInputStream(zf.getInputStream(ze));
+
+		BufferedReader br = null;
+		try {
+			BufferedWriter bw = new BufferedWriter(new FileWriter(file));
+			br = new BufferedReader(new InputStreamReader(
+					new BufferedInputStream(zf.getInputStream(ze))));
+			while ((b = br.readLine()) != null) {
+				bw.write(b);
+				bw.newLine();
+				bw.flush();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				br.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		// while ((b = inputStream.read(b)) > 0) {
+		// outputStream.write(b, 0, b);
+		// }
+		// inputStream.close();
+		// outputStream.close();
+
+		// RemoteFile rf = RemoteFile.createEmptyRemoteFile(fileDB, namespace);
+		// rf.setFileUploaded(name, file, new ObjectId());
+		// fileSet.add(rf, context);
+
+		// doc.reload();
+		// fileSet.saveServerFileToDB();
+		// doc.setValue(Document.F_VAULT, fileSet.getUpdatedData(), null, true);
+		// doc.doSave(context);
+		// file.delete();
 	}
 
 	private ObjectId createFolder(Organization org, IContext context,
