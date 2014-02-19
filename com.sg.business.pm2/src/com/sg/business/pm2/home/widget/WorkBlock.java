0bf818d7ca665260f96aed8d417d05411a3f6951
Administrator <Administrator@192.168.244.128>
2014-02-19 11:34:27 +0800

    Signed-off-by: Administrator <Administrator@192.168.244.128>

---
+++ com.sg.business.pm2/src/com/sg/business/pm2/home/widget/WorkBlock.java
@@ -0,0 +1,14 @@
+package com.sg.business.pm2.home.widget;
+
+import org.eclipse.swt.widgets.Composite;
+
+import com.sg.widgets.block.Block;
+
+public class WorkBlock extends Block {
+
+	public WorkBlock(Composite parent) {
+		super(parent);
+		// TODO Auto-generated constructor stub
+	}
+
+}
