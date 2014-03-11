48bbdfada3e801ec51711a377b541daf47adec8b
ghuazh <ghuazh@gmail.com>
2014-03-11 11:58:26 +0800

    Signed-off-by: ghuazh <ghuazh@gmail.com>

--- com.sg.sales/src/com/sg/sales/ui/block/CompanyContentBlock.java
+++ com.sg.sales/src/com/sg/sales/ui/block/CompanyContentBlock.java
@@ -2,12 +2,19 @@ package com.sg.sales.ui.block;
 
 import org.eclipse.swt.widgets.Composite;
 
+import com.sg.sales.model.Company;
 import com.sg.widgets.block.BusinessContentBlock;
 
 public class CompanyContentBlock extends BusinessContentBlock {
 
 	public CompanyContentBlock(Composite parent) {
 		super(parent);
+	}
+	
+	@Override
+	protected String getBodyText(boolean hoverMask) {
+		Company company = (Company) getInput();
+		return company.getLevel();
 	}
 
 }
