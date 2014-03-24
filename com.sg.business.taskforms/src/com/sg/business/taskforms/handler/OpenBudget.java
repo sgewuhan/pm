fc642c85773632d4f73c8cf9bb7c8f745bba39bf
yangjun <Administrator@192.168.244.128>
2014-03-25 02:06:29 +0800

    Signed-off-by: yangjun <Administrator@192.168.244.128>

---
+++ com.sg.business.taskforms/src/com/sg/business/taskforms/handler/OpenBudget.java
@@ -0,0 +1,38 @@
+package com.sg.business.taskforms.handler;
+
+import org.bson.types.ObjectId;
+import org.eclipse.ui.forms.IFormPart;
+
+import com.mobnut.db.model.ModelService;
+import com.mobnut.db.model.PrimaryObject;
+import com.sg.business.model.Project;
+import com.sg.business.model.TaskForm;
+import com.sg.widgets.part.CurrentAccountContext;
+import com.sg.widgets.part.editor.DataObjectDialog;
+import com.sg.widgets.part.editor.PrimaryObjectEditorInput;
+import com.sg.widgets.part.editor.fields.value.IFieldActionHandler;
+
+public class OpenBudget implements IFieldActionHandler {
+
+	public OpenBudget() {
+	}
+
+	@Override
+	public Object run(IFormPart abstractFieldPart,
+			PrimaryObjectEditorInput input) {
+		PrimaryObject po = input.getData();
+		if (po instanceof TaskForm) {
+			TaskForm taskForm = (TaskForm) po;
+			try {
+				String project_id = (String) taskForm.getProcessInstanceVarible("project_id", new CurrentAccountContext()); //$NON-NLS-1$
+				ObjectId projectid = new ObjectId(project_id);
+				Project project = ModelService.createModelObject(Project.class, projectid);
+				DataObjectDialog.openDialog(project,Project.EDITOR_PROJECT_FINANCIAL, false, null);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return null;
+	}
+
+}
