db2af07350ea82f62aab3e19b8eecc3414a5932b
yangjun <Administrator@192.168.244.128>
2014-04-22 15:36:06 +0800

    Signed-off-by: yangjun <Administrator@192.168.244.128>

---
+++ com.tmt.document/src/com/tmt/document/tb/ProdectProposalSet.java
@@ -0,0 +1,47 @@
+package com.tmt.document.tb;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.mobnut.db.model.DataSet;
+import com.mobnut.db.model.ModelService;
+import com.mobnut.db.model.PrimaryObject;
+import com.mongodb.DBObject;
+import com.sg.business.model.Document;
+import com.sg.business.model.IModelConstants;
+import com.sg.widgets.commons.dataset.MasterDetailDataSetFactory;
+
+public class ProdectProposalSet extends MasterDetailDataSetFactory {
+
+	private static final String PRODUCT = "product";
+
+	public ProdectProposalSet() {
+		super(IModelConstants.DB, IModelConstants.C_DOCUMENT);
+	}
+
+	@Override
+	protected String getDetailCollectionKey() {
+		return null;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public DataSet getDataSet() {
+		if (master != null) {
+			if (master instanceof Document) {
+				Document document = (Document) master;
+				List<PrimaryObject> productList = new ArrayList<PrimaryObject>();
+				List<DBObject> product = (List<DBObject>) document
+						.getListValue(PRODUCT);
+				for (DBObject object : product) {
+					PrimaryObject po = ModelService.createModelObject(object, PrimaryObject.class);
+					productList.add(po);
+				}
+
+				return new DataSet(productList);
+			}
+		}
+		return super.getDataSet();
+	}
+
+}
