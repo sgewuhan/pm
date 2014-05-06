89bcabb4dbe7d7add5c56f993485aba7d68b412f
LeiChengyang <lcy@yaozheng.com.cn>
2014-05-06 21:47:34 +0800

    Signed-off-by: LeiChengyang <lcy@yaozheng.com.cn>

--- com.tmt.gs/src/com/tmt/gs/dataset/PrintDocOfGS.java
+++ com.tmt.gs/src/com/tmt/gs/dataset/PrintDocOfGS.java
@@ -3,64 +3,44 @@ package com.tmt.gs.dataset;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.mobnut.db.model.DataSet;
 import com.mobnut.db.model.IContext;
 import com.mobnut.db.model.PrimaryObject;
+import com.mobnut.db.model.mongodb.SingleDBCollectionDataSetFactory;
 import com.sg.business.model.Document;
 import com.sg.business.model.IModelConstants;
 import com.sg.business.model.Project;
-import com.sg.business.model.TaskForm;
 import com.sg.business.model.User;
 import com.sg.business.model.Work;
 import com.sg.business.model.toolkit.UserToolkit;
-import com.sg.widgets.commons.dataset.MasterDetailDataSetFactory;
 import com.sg.widgets.part.CurrentAccountContext;
 
-public class PrintDocOfGS extends MasterDetailDataSetFactory{
-	
+public class PrintDocOfGS extends SingleDBCollectionDataSetFactory {
+
 	public IContext context;
 	private User user;
 
-	public PrintDocOfGS(String dbName, String collectionName) {
+	public PrintDocOfGS() {
 		super(IModelConstants.DB, IModelConstants.C_DOCUMENT);
-		context=new CurrentAccountContext();
+		context = new CurrentAccountContext();
 		String userId = context.getAccountInfo().getUserId();
-		user=UserToolkit.getUserById(userId);
-	}
-
-	public DataSet getDataSet(){
-		if(master!=null){
-			if(master instanceof TaskForm){
-				List<PrimaryObject> doclist=new ArrayList<PrimaryObject>();
-				List<PrimaryObject> chargeProject = user.getChargeProject(null);
-				for (int i = 0; i < chargeProject.size(); i++) {
-					PrimaryObject po = chargeProject.get(i);
-					if(po instanceof Project){
-						Project project=(Project) po;
-						Work work = project.getWBSRoot();
-						List<PrimaryObject> documents = work.getDeliverableDocuments();
-						for (int j = 0; j < documents.size(); j++) {
-							PrimaryObject document = documents.get(j);
-							if(document instanceof Document){
-								Document doc=(Document) document;
-								String lifecycle = doc.getLifecycle();
-								if("STATUS_RELEASED_ID".equals(lifecycle)){
-									doclist.add(doc);
-									return new DataSet(doclist);
-								}
-							}
-						}
-						
+		user = UserToolkit.getUserById(userId);
+		List<PrimaryObject> doclist = new ArrayList<PrimaryObject>();
+		List<PrimaryObject> projectlist = user.getChargeProject(null);
+		for (PrimaryObject po : projectlist) {
+			Project project = (Project) po;
+			List<PrimaryObject> worklist = project.getChildrenWork();
+			for (PrimaryObject works : worklist) {
+				Work work = (Work) works;
+				List<PrimaryObject> documents = work.getDeliverableDocuments();
+				for (int i = 0; i < documents.size(); i++) {
+					PrimaryObject docs = documents.get(i);
+					Document document = (Document) docs;
+					if ("STATUS_RELEASED_ID".equals(document.getLifecycle())) {
+						doclist.add(document);
 					}
 				}
 			}
 		}
-		return super.getDataSet();
-	}
-
-	@Override
-	protected String getDetailCollectionKey() {
-		return null;
 	}
 
 }
