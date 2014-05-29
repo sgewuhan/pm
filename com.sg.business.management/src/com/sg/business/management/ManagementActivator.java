7c814bc8885ddbb4f220a35276151d1eff66222f
yangjun1985 <Administrator@WIN-U9FETHCVP2R>
2014-05-29 20:18:40 +0800

    Signed-off-by: yangjun1985 <Administrator@WIN-U9FETHCVP2R>

--- com.sg.business.management/src/com/sg/business/management/ManagementActivator.java
+++ com.sg.business.management/src/com/sg/business/management/ManagementActivator.java
@@ -1,13 +1,7 @@
 package com.sg.business.management;
 
-import java.util.Iterator;
-
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
-
-import com.sg.widgets.Widgets;
-import com.sg.widgets.registry.DataEditorRegistry;
-import com.sg.widgets.registry.config.Configurator;
 
 /**
  * The activator class controls the plug-in life cycle
@@ -19,8 +13,7 @@ public class ManagementActivator extends AbstractUIPlugin {
 
 	// The shared instance
 	private static ManagementActivator plugin;
-	
-	
+
 	/**
 	 * The constructor
 	 */
@@ -29,21 +22,22 @@ public class ManagementActivator extends AbstractUIPlugin {
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext
+	 * )
 	 */
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
-		/*DataEditorRegistry registry = Widgets.getEditorRegistry();
-		Iterator<Configurator> iterator = registry.getConfigurators().iterator();
-		while(iterator.hasNext()){
-			System.out.println(iterator.next().getId());
-		}*/
 	}
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext
+	 * )
 	 */
 	public void stop(BundleContext context) throws Exception {
 		plugin = null;
