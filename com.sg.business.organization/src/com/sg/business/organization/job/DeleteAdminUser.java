c4490d27206f8e85ca760679e65c984d51d16a4d
yangjun1985 <Administrator@WIN-U9FETHCVP2R>
2014-06-04 10:44:18 +0800

    Signed-off-by: yangjun1985 <Administrator@WIN-U9FETHCVP2R>

--- com.sg.business.organization/src/com/sg/business/organization/job/DeleteAdminUser.java
+++ com.sg.business.organization/src/com/sg/business/organization/job/DeleteAdminUser.java
@@ -1,6 +1,7 @@
 package com.sg.business.organization.job;
 
 import org.bson.types.BasicBSONList;
+import org.bson.types.ObjectId;
 
 import com.mobnut.admin.schedual.registry.ISchedualJobRunnable;
 import com.mobnut.db.DBActivator;
@@ -11,20 +12,21 @@ import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
 import com.sg.business.model.IModelConstants;
 import com.sg.business.model.Project;
+import com.sg.business.model.ProjectMonthData;
 
 public class DeleteAdminUser implements ISchedualJobRunnable {
 
 	@Override
 	public boolean run() throws Exception {
-		DBCollection col = getCol(IModelConstants.C_PROJECT);
-		DBCursor cursor = col.find(new BasicDBObject().append(
+		DBCollection ProjectCol = getCol(IModelConstants.C_PROJECT);
+		DBCursor projectCursor = ProjectCol.find(new BasicDBObject().append(
 				Project.F_PARTICIPATE,
 				new BasicDBObject().append("$in", new String[] { "yangjun",
 						"zhonghua", "jinxt"
 
 				})));
-		while (cursor.hasNext()) {
-			DBObject dbObject = cursor.next();
+		while (projectCursor.hasNext()) {
+			DBObject dbObject = projectCursor.next();
 			Project project = ModelService.createModelObject(dbObject,
 					Project.class);
 			BasicBSONList list = (BasicBSONList) project
@@ -39,12 +41,33 @@ public class DeleteAdminUser implements ISchedualJobRunnable {
 			if (!"jinxt".equals(chargerId)) {
 				list.remove("jinxt");
 			}
-			col.update(new BasicDBObject().append(Project.F__ID,
-					project.get_id()), new BasicDBObject().append("$set",
-					new BasicDBObject().append(Project.F_PARTICIPATE, list)),
-					false, false);
+			ProjectCol
+					.update(new BasicDBObject().append(Project.F__ID,
+							project.get_id()), new BasicDBObject().append(
+							"$set", new BasicDBObject().append(
+									Project.F_PARTICIPATE, list)), false, false);
 		}
+		DBCollection projectMonthDataCol = getCol(IModelConstants.C_PROJECT_MONTH_DATA);
+		DBCursor projectMonthDataCursor = projectMonthDataCol.find();
+		while (projectMonthDataCursor.hasNext()) {
+			DBObject dbObject = projectMonthDataCursor.next();
+			ProjectMonthData projectMonthData = ModelService.createModelObject(
+					dbObject, ProjectMonthData.class);
+			ObjectId project_id = (ObjectId) projectMonthData
+					.getValue(ProjectMonthData.F_PROJECTID);
+			ObjectId projectMonthDataId = projectMonthData.get_id();
+			Integer month = projectMonthData.getMonth();
+			Integer year = projectMonthData.getYear();
+			System.out.println("" + year + " " + month + " " + project_id);
+			projectMonthDataCol.remove(new BasicDBObject()
+					.append(ProjectMonthData.F_MONTH, month)
+					.append(ProjectMonthData.F_YEAR, year)
+					.append(ProjectMonthData.F_PROJECTID, project_id)
+					.append(ProjectMonthData.F__ID,
+							new BasicDBObject().append("$ne",
+									projectMonthDataId)));
 
+		}
 		return true;
 	}
 
